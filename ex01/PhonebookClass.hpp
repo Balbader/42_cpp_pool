db0dcd1341bd84a5e37e2f762dc1fa770bb875c7
balbader <basilalbapro@gmail.com>
2023-10-22 18:05:57 +0200

    WIP

--- ex01/PhonebookClass.hpp
+++ ex01/PhonebookClass.hpp
@@ -10,4 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
 
+#endif // !PHONEBOOK_H
