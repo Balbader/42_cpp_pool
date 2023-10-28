d26a0cbe03cc06bb1df6666fd6b609eb25a6110a
Basil ALBADER <baalbade@student.42.fr>
2023-10-28 16:56:08 +0200

    WIP

--- ex01/contact.cpp
+++ ex01/contact.cpp
@@ -10,4 +10,50 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ContactClass.hpp"
+#include <cstring>
 
+void Contact::ftSetFirstName() {
+	std::cout << "Enter New Contact First Name:" << std::endl;
+	std::cin >> firstName;
+};
+
+void Contact::ftGetFirstName() {
+	return firstName;
+};
+
+void Contact::ftSetLastName() {
+	std::cout << "Enter New Contact Last Name:" << std::endl;
+	std::cin >> lastName;
+};
+
+void Contact::ftGetLastName() {
+	return lastName;
+};
+
+void Contact::ftSetNickname() {
+	std::cout << "Enter New Contact Nickname:" << std::endl;
+	std::cin >> nickname;
+};
+
+void Contact::ftGetNickname() {
+	return nickname;
+};
+
+void Contact::ftSetPhoneNumber() {
+	std::cout << "Enter New Contact 8 Digits Phone Number:" << std::endl;
+	std::cin >> phoneNumber;
+};
+
+void Contact::ftGetPhoneNumber() {
+	return phoneNumber;
+};
+
+void Contact::ftSetDarkestSecret() {
+	std::cout << "Enter New Contact Darkest Secret:" << std::endl;
+	std::cin >> darkestSecret;
+}
+
+void Contact::ftGetDarkestSecret() {
+	return darkestSecret;
+}
