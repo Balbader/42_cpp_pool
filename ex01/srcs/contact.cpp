3a558950cd57446b6bd35780d97b4b2768ee0329
Basil ALBADER <baalbade@student.42.fr>
2023-10-30 07:23:44 +0100

    contact.cpp

---
+++ ex01/srcs/contact.cpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 15:12:33 by baalbade          #+#    #+#             */
+/*   Updated: 2023/10/22 15:12:35 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ContactClass.hpp"
+#include <string>
+
+//============================================================ Contact firstName
+void Contact::ftSetFirstName() {
+	std::cout << "Enter New Contact First Name:" << std::endl;
+	std::cin >> firstName;
+};
+std::string Contact::ftGetFirstName() {
+	return firstName;
+};
+
+//============================================================ Contact lastName
+void Contact::ftSetLastName() {
+	std::cout << "Enter New Contact Last Name:" << std::endl;
+	std::cin >> lastName;
+};
+std::string Contact::ftGetLastName() {
+	return lastName;
+};
+
+//============================================================ Contact nickname
+void Contact::ftSetNickname() {
+	std::cout << "Enter New Contact Nickname:" << std::endl;
+	std::cin >> nickname;
+};
+std::string Contact::ftGetNickname() {
+	return nickname;
+};
+
+//============================================================ Contact phoneNumber
+void Contact::ftSetPhoneNumber() {
+	std::cout << "Enter New Contact 8 Digits Phone Number:" << std::endl;
+	std::cin >> phoneNumber;
+};
+std::string Contact::ftGetPhoneNumber() {
+	return phoneNumber;
+};
+
+//============================================================ Contact darkestSecret
+void Contact::ftSetDarkestSecret() {
+	std::cout << "Enter New Contact Darkest Secret:" << std::endl;
+	std::cin >> darkestSecret;
+}
+std::string Contact::ftGetDarkestSecret() {
+	return darkestSecret;
+}
+
+//============================================================ Run Program function
+void Contact::ftRunProgram() {
+
+	ftSetFirstName();
+	ftSetLastName();
+	ftSetNickname();
+	ftSetPhoneNumber();
+	ftSetDarkestSecret();
+
+	std::cout << "Contact Name: " << ftGetFirstName() << " " << ftGetLastName() << std::endl;
+	std::cout << "Contact Nickname: " << ftGetFirstName() << std::endl;
+	std::cout << "Contact Phone Number: " << ftGetPhoneNumber() << std::endl;
+	std::cout << "Contact Darkest Secret: " << ftGetDarkestSecret() << std::endl;
+}
