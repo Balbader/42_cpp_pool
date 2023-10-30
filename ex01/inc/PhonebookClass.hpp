c44867e3d35fdbf940174a9944c872c3b1edc4bb
Basil ALBADER <baalbade@student.42.fr>
2023-10-30 07:23:24 +0100

    .hpp files

---
+++ ex01/inc/PhonebookClass.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 15:08:03 by baalbade          #+#    #+#             */
+/*   Updated: 2023/10/22 15:08:06 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+#include "ContactClass.hpp"
+
+class Phonebook {
+public:
+  void getUserInfo(Contact);
+}
+
+void Phonebook::getUserInfo(Contact UserInfo) {
+}
+
+#endif // !PHONEBOOK_H
