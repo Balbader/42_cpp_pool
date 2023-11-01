3a5752502bbeddc3a3bd423cde6f7ad5625823f3
Basil ALBADER <baalbade@student.42.fr>
2023-11-01 18:58:06 +0100

    WIP

--- ex01/inc/define.hpp
+++ ex01/inc/define.hpp
@@ -1,9 +1,19 @@
 #ifndef DEFINE_HPP
 #define DEFINE_HPP
 
+// Colors
 #define RED "\033[0;31m"
 #define GREEN "\033[0;32m"
 #define BLUE "\033[0;34m"
 #define RESET "\033[0;0m"
+
+// Error Messages
+#define ENTERFIRST "Enter New Contact's First Name:"
+#define ENTERLAST "Enter New Contact's Last Name:"
+#define ENTERNICK "Enter New Contact's Nickname:"
+#define ENTERPHONE "Enter New Contact's Phone Number:"
+#define ENTERDARK "Enter New Contact's Darkest Secret:"
+
+#define EMPTY "Field is empty!\nPlease make sure to add the contact's "
 
 #endif // !DEFINE_HPP
