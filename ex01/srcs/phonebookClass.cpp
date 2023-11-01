f75b22a7382b28495fe06b4a52091f8186f1752d
Basil ALBADER <baalbade@student.42.fr>
2023-11-01 17:19:45 +0100

    WIP

--- ex01/srcs/phonebookClass.cpp
+++ ex01/srcs/phonebookClass.cpp
@@ -1,11 +1,18 @@
 #include "PhonebookClass.hpp"
+#include "define.hpp"
+#include <iomanip>
+#include <iostream>
 
 Phonebook::Phonebook() {
-  std::cout << GREEN << "Phonebook Constructor Called!" << RESET << std::endl;
+  std::cout << GREEN
+            << "----------------------------> Phonebook Constructor Called!"
+            << RESET << std::endl;
 };
 
 Phonebook::~Phonebook() {
-  std::cout << GREEN << "Phonebook Destructor Called!" << RESET << std::endl;
+  std::cout << RED
+            << "----------------------------> Phonebook Destructor Called!"
+            << RESET << std::endl;
 };
 
 void Phonebook::RunProgram() {
