076878a82b2c27c3974cdeffaca9c3bc64d275c1
Basil ALBADER <baalbade@student.42.fr>
2023-11-02 08:35:55 +0100

    WIP

--- ex01/srcs/phonebookClass.cpp
+++ ex01/srcs/phonebookClass.cpp
@@ -3,13 +3,13 @@
 #include <iomanip>
 #include <iostream>
 
-Phonebook::Phonebook() {
+Phonebook::Phonebook(void) {
   std::cout << GREEN
             << "----------------------------> Phonebook Constructor Called!"
             << RESET << std::endl;
 };
 
-Phonebook::~Phonebook() {
+Phonebook::~Phonebook(void) {
   std::cout << RED
             << "----------------------------> Phonebook Destructor Called!"
             << RESET << std::endl;
@@ -20,9 +20,43 @@ void Phonebook::printContact() {
   newContact.printContactInfo();
 }
 
-void Phonebook::RunProgram() {
-
+void Phonebook::runProgram() {
   Contact newContact;
-
   newContact.GetContactInfo();
 };
+
+void runProgram();
+void addContact(Contact newContact);
+void printContact(int index);
+void printContactList(void);
+
+bool Phonebook::add_contact(Contact contact) {
+  if (_contacts_count >= 8)
+    return false;
+  _contacts[_contacts_count] = contact;
+  _contacts_count++;
+  return true;
+}
+
+void Phonebook::print_selected_contact(int index) {
+
+  if (index < 0 || index > 7 || index >= this->_contacts_count) {
+    std::cout << "Sorry, this index is invalid." << std::endl;
+    return;
+  }
+  _contacts[index].print_all_contact_fields();
+}
+
+void Phonebook::print_list(void) {
+
+  std::cout << std::setw(10) << "index"
+            << "|"
+            << "first name"
+            << "|"
+            << "last name"
+            << "|"
+            << "nickname" << std::endl;
+
+  for (int i = 0; i < _contacts_count; i++)
+    _contacts[i].print_contact(i);
+}
