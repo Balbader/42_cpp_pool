77270c78d6e1d3f68236dcb093ddd003de4e7c3f
Basil ALBADER <baalbade@student.42.fr>
2023-11-02 08:50:39 +0100

    WIP

--- ex01/srcs/phonebookClass.cpp
+++ ex01/srcs/phonebookClass.cpp
@@ -15,44 +15,8 @@ Phonebook::~Phonebook(void) {
             << RESET << std::endl;
 };
 
-bool Phonebook::addContact(Contact contact) {
-  if (_contactCount >= 8)
-    return false;
-  _contactList[_contactsCount] = contact;
-  _contactsCount++;
-  return true;
-}
+void Phonebook::addContact(Contact contact) {}
 
-void Phonebook::printContact() {
-  Contact newContact;
-  newContact.printContactInfo();
-}
+void Phonebook::printContact(int index) {}
 
-void Phonebook::printContact(int index) {
-
-  if (index < 0 || index > 7 || index >= this->_contactsCount) {
-    std::cout << "Sorry, this index is invalid." << std::endl;
-    return;
-  }
-  _contactList[index]
-      .print_all_contact_fields(); // ------------------------------> !!!!!
-}
-
-void Phonebook::printContactList(void) {
-
-  std::cout << std::setw(10) << "index"
-            << "|"
-            << "first name"
-            << "|"
-            << "last name"
-            << "|"
-            << "nickname" << std::endl;
-
-  for (int i = 0; i < _contactCount; i++)
-    _contactList[i].printContact(i);
-}
-
-void Phonebook::runProgram() {
-  Contact newContact;
-  newContact.GetContactInfo();
-};
+void Phonebook::printContactList(void) {}
