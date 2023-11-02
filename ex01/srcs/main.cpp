dd47fe2e4aa0f8830ea674916670c0130227f681
Basil ALBADER <baalbade@student.42.fr>
2023-11-02 08:35:55 +0100

    WIP

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -2,7 +2,27 @@
 #include "PhonebookClass.hpp"
 
 int main(void) {
+  std::string input;
   Phonebook newPhonebook;
-  newPhonebook.runProgram();
+
+  while (true) {
+    std::cout << "Please enter your command: " << std::endl;
+    std::cin >> input;
+    if (input == "EXIT")
+      break;
+    else if (input == "ADD") {
+      Contact newContact = GetContactInfo();
+      if (!my_phonebook.add_contact(new_contact))
+        std::cout << "Contact list is full" << std::endl;
+    } else if (input == "SEARCH") {
+      newPhonebook.printContactList();
+      std::cout
+          << "Please enter the index you'd like to see more information of"
+          << std::endl;
+      std::cin >> input;
+      int index = (int)(input.at(0) - 48);
+      newPhonebook.print_selected_contact(index);
+    }
+  }
   return (0);
 }
