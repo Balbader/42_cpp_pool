7a963eca10026a55a73dfddd69c540ab66e8a6f0
Basil ALBADER <baalbade@student.42.fr>
2023-11-04 20:26:15 +0100

    WIP

--- ex01/inc/PhonebookClass.hpp
+++ ex01/inc/PhonebookClass.hpp
@@ -7,15 +7,16 @@
 #include <iostream>
 
 class Phonebook {
-  Contact _contactList[8];
+  Contact _contactList[3];
   int _contactCount;
   std::string _checkInput(std::string);
   void _printOptions(void);
+  void _keepGoing(void);
 
 public:
   Phonebook(void);
   void runProgram(void);
-  void addContact(int, int);
+  Contact addContact(int, int);
   void printContact(int index);
   void printContactList(void);
   ~Phonebook(void);
