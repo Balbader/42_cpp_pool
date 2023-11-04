d30c172aec4af2db9cfc5caa414a7241e9e24cdd
Basil ALBADER <baalbade@student.42.fr>
2023-11-04 17:13:19 +0100

    WIP

--- ex01/srcs/phonebookClass.cpp
+++ ex01/srcs/phonebookClass.cpp
@@ -7,13 +7,18 @@ Phonebook::Phonebook(void) { return; };
 
 Phonebook::~Phonebook(void) { return; };
 
-void Phonebook::addContact(int i) {
+void Phonebook::addContact(Contact _contactList) {
   Contact newContact;
+  int j;
 
   std::cout << std::endl;
   newContact.getContactInfo();
   _contactList[i] = newContact;
-  newContact.printContactInfo(_contactList[i], i);
+  std::cout << std::endl;
+  for (int j = 0; j < 8; ++j) {
+    newContact.printContactInfo(_contactList[j], j + 1);
+  }
+  // newContact.printContactInfo(_contactList[i], i);
 }
 
 void Phonebook::printContact(int index) { (void)index; }
@@ -21,14 +26,18 @@ void Phonebook::printContact(int index) { (void)index; }
 void Phonebook::printContactList(void) {}
 
 void Phonebook::runProgram(void) {
+
   Phonebook newBook;
   std::string input;
 
   _printOptions();
+
   std::getline(std::cin, input);
+
   input = _checkInput(input);
+
   if (input == "ADD") {
-    newBook.addContact(0);
+    newBook.addContact();
   } else if (input == "SEARCH") {
     std::cout << RED << "SEARCH command entered!" << RESET << std::endl;
     return;
