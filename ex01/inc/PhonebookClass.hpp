28f382a137b8b90c4c154073da4641bf89eaf6a3
Basil ALBADER <baalbade@student.42.fr>
2023-11-09 17:10:27 +0100

    WIP

--- ex01/inc/PhonebookClass.hpp
+++ ex01/inc/PhonebookClass.hpp
@@ -24,7 +24,7 @@ public:
   Phonebook(void);
   void runProgram(void);
   int addContact(int);
-  int printContact(char, int);
+  void printContact(char, int);
   void printContactList(int);
   ~Phonebook(void);
 };
