0f680353992bb63704295d36a7a1d6f830d1543e
Basil ALBADER <baalbade@student.42.fr>
2023-11-09 09:36:38 +0100

    SearchClass.hpp

---
+++ ex01/inc/SearchClass.hpp
@@ -0,0 +1,14 @@
+#ifndef SEARCHCLASS_HPP
+#define SEARCHCLASS_HPP
+
+#include "ContactClass.hpp"
+#include "PhonebookClass.hpp"
+#include <iomanip>
+#include <iostream>
+
+class Search {
+  void searchContact(Contact);
+  std::string convertContact(std::string);
+}
+
+#endif // !SEARCHCLASS_HPP
