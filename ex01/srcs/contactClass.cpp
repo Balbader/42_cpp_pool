623f97d8025721b2ff7897a712238d0e39f0cf50
Basil ALBADER <baalbade@student.42.fr>
2023-11-10 13:14:08 +0100

    WIP

--- ex01/srcs/contactClass.cpp
+++ ex01/srcs/contactClass.cpp
@@ -10,10 +10,50 @@ std::string Contact::getInput(std::string enter, std::string attr,
   std::cout << BLUE << enter << attr << RESET << std::endl;
   std::getline(std::cin, input);
   while (input.length() == 0) {
-    std::cout << empty << attr << std::endl;
-    std::cout << std::endl;
+    std::cout << RED << "Error encountered!\n" << RESET << std::endl;
+    std::cout << empty << PLEASE << attr << std::endl;
+    // std::cout << std::endl;
     std::cout << BLUE << enter << attr << RESET << std::endl;
     std::getline(std::cin, input);
+  }
+  return input;
+}
+//============================================================ Check number
+int Contact::checkNumber(std::string input) {
+  for (int i = 0; i < (int)input.length(); ++i) {
+    if (!std::isdigit(input[i]))
+      return (1);
+  }
+  return (0);
+}
+//============================================================ Get number
+std::string Contact::getNumber(std::string enter, std::string attr,
+                               std::string empty, std::string input) {
+  std::cout << BLUE << enter << attr << RESET << std::endl;
+  std::getline(std::cin, input);
+  while (input.length() == 0 || input.length() > 10 ||
+         checkNumber(input) == 1) {
+    if (input.length() == 0) {
+      std::cout << RED << empty << RESET << attr << std::endl;
+      std::cout << std::endl;
+      std::cout << BLUE << enter << attr << RESET << std::endl;
+      std::getline(std::cin, input);
+    } else if (input.length() > 10) {
+      std::cout << std::endl;
+      std::cout << RED << "Error encountered!\n" << RESET << std::endl;
+      std::cout << "Wrong data format. Phone number can only contain" << RED
+                << " 10 digits" << RESET << "!" << BLUE
+                << "\nPlease enter valid number:" << RESET << std::endl;
+      std::getline(std::cin, input);
+
+    } else if (checkNumber(input) == 1) {
+      std::cout << std::endl;
+      std::cout << RED << "Error encountered!\n" << RESET << std::endl;
+      std::cout << "Wrong data format. Phone number can only contain" << RED
+                << " digits" << RESET << "!" << BLUE
+                << "\nPlease enter valid number:" << RESET << std::endl;
+      std::getline(std::cin, input);
+    }
   }
   return input;
 }
@@ -41,7 +81,7 @@ std::string Contact::getNickname(void) const { return _nickname; }
 
 //============================================================ Contact Phone
 void Contact::setPhoneNumber(std::string input) {
-  _phoneNumber = getInput(ENTER, PHONE, EMPTY, input);
+  _phoneNumber = getNumber(ENTER, PHONE, EMPTY, input);
   return;
 }
 std::string Contact::getPhoneNumber(void) const { return _phoneNumber; }
@@ -69,8 +109,9 @@ void Contact::getContactInfo(void) {
 
 void Contact::printContactInfo(Contact newContact, int index) {
   std::cout << std::endl;
-  std::cout << std::setw(10) << index << " | " << newContact.getFirstName()
-            << " | " << newContact.getLastName() << " | "
+  std::cout << std::setw(10) << index << " | " << std::setw(10)
+            << newContact.getFirstName() << " | " << std::setw(10)
+            << newContact.getLastName() << " | " << std::setw(10)
             << newContact.getNickname() << std::endl;
   return;
 }
