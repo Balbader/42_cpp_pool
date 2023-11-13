2ca7d64b25b9f335f21fb17bec70fac40e7455d3
Basil ALBADER <baalbade@student.42.fr>
2023-11-13 18:19:58 +0100

    WIP

--- ex01/srcs/contactClass.cpp
+++ ex01/srcs/contactClass.cpp
@@ -1,18 +1,31 @@
 #include "ContactClass.hpp"
-#include "PhonebookClass.hpp"
 
-Contact::Contact(void) { return; }
+//----------------------------------------------------------------> Constructor
+Contact::Contact(void) {}
+Contact::~Contact(void) {}
 
-Contact::~Contact(void) { return; }
-
-//============================================================ Exit message
+//----------------------------------------------------------------> Err message
 void Contact::printExit(void) {
   std::cout << std::endl
-            << "Exiting the phonebook. All contacts are lost forever."
-            << std::endl;
+            << RED << "Exiting the phonebook. All contacts are lost forever."
+            << RESET << std::endl;
 }
 
-//============================================================ Get Input
+void Contact::printDigitErrorMessage(void) {
+  std::cout << std::endl << RED << "Error encountered!\n" << RESET << std::endl;
+  std::cout << "Wrong data format. Phone number must contain" << RED
+            << " 10 digits" << RESET << "!" << BLUE
+            << "\nPlease enter valid number:" << RESET << std::endl;
+}
+
+void Contact::printCharErrorMessage(void) {
+  std::cout << std::endl << RED << "Error encountered!\n" << RESET << std::endl;
+  std::cout << "Wrong data format. Phone number can only contain" << RED
+            << " digits" << RESET << "!" << BLUE
+            << "\nPlease enter valid number:" << RESET << std::endl;
+}
+
+//------------------------------------------------------------------> Get Input
 std::string Contact::getInput(std::string enter, std::string attr,
                               std::string empty, std::string input) {
   std::cout << BLUE << enter << attr << RESET << std::endl;
@@ -25,7 +38,8 @@ std::string Contact::getInput(std::string enter, std::string attr,
   }
   return input;
 }
-//============================================================ Check number
+
+//---------------------------------------------------------------> Check number
 int Contact::checkNumber(std::string input) {
   for (int i = 0; i < (int)input.length(); ++i) {
     if (!std::isdigit(input[i]))
@@ -33,7 +47,8 @@ int Contact::checkNumber(std::string input) {
   }
   return (0);
 }
-//============================================================ Get number
+
+//-----------------------------------------------------------------> Get number
 std::string Contact::getNumber(std::string enter, std::string attr,
                                std::string empty, std::string input) {
   std::cout << BLUE << enter << attr << RESET << std::endl;
@@ -48,24 +63,17 @@ std::string Contact::getNumber(std::string enter, std::string attr,
         printExit();
         break;
       }
-    } else if (input.length() > 10) {
-      std::cout << std::endl
-                << RED << "Error encountered!\n"
-                << RESET << std::endl;
-      std::cout << "Wrong data format. Phone number must contain" << RED
-                << " 10 digits" << RESET << "!" << BLUE
-                << "\nPlease enter valid number:" << RESET << std::endl;
+    }
+    if (input.length() > 10) {
+      printDigitErrorMessage();
       std::getline(std::cin, input);
       if (std::cin.eof()) {
         printExit();
         break;
       }
-    } else if (checkNumber(input) == 1) {
-      std::cout << std::endl;
-      std::cout << RED << "Error encountered!\n" << RESET << std::endl;
-      std::cout << "Wrong data format. Phone number can only contain" << RED
-                << " digits" << RESET << "!" << BLUE
-                << "\nPlease enter valid number:" << RESET << std::endl;
+    }
+    if (checkNumber(input) == 1) {
+      printCharErrorMessage();
       std::getline(std::cin, input);
       if (std::cin.eof()) {
         printExit();
@@ -76,37 +84,7 @@ std::string Contact::getNumber(std::string enter, std::string attr,
   return input;
 }
 
-//============================================================ Contact firstname
-void Contact::setFirstName(std::string input) {
-  _firstName = getInput(ENTER, FIRST, EMPTY, input);
-}
-std::string Contact::getFirstName(void) const { return _firstName; }
-
-//============================================================ Contact lastName
-void Contact::setLastName(std::string input) {
-  _lastName = getInput(ENTER, LAST, EMPTY, input);
-}
-std::string Contact::getLastName(void) const { return _lastName; }
-
-//============================================================ Contact Nickname
-void Contact::setNickname(std::string input) {
-  _nickname = getInput(ENTER, NICK, EMPTY, input);
-}
-std::string Contact::getNickname(void) const { return _nickname; }
-
-//============================================================ Contact Phone
-void Contact::setPhoneNumber(std::string input) {
-  _phoneNumber = getNumber(ENTER, PHONE, EMPTY, input);
-}
-std::string Contact::getPhoneNumber(void) const { return _phoneNumber; }
-
-//============================================================ Contact Secret
-void Contact::setDarkestSecret(std::string input) {
-  _darkestSecret = getInput(ENTER, SECRET, EMPTY, input);
-}
-std::string Contact::getDarkestSecret(void) const { return _darkestSecret; }
-
-//============================================================ Contact Info
+//---------------------------------------------------------------> Contact Info
 void Contact::getContactInfo(void) {
 
   std::string input = "";
@@ -126,3 +104,33 @@ void Contact::printContactInfo(Contact newContact, int index) {
             << newContact.getLastName() << " | " << std::setw(10)
             << newContact.getNickname() << std::endl;
 }
+
+//----------------------------------------------------------> Contact firstname
+void Contact::setFirstName(std::string input) {
+  _firstName = getInput(ENTER, FIRST, EMPTY, input);
+}
+std::string Contact::getFirstName(void) const { return _firstName; }
+
+//-----------------------------------------------------------> Contact lastName
+void Contact::setLastName(std::string input) {
+  _lastName = getInput(ENTER, LAST, EMPTY, input);
+}
+std::string Contact::getLastName(void) const { return _lastName; }
+
+//-----------------------------------------------------------> Contact Nickname
+void Contact::setNickname(std::string input) {
+  _nickname = getInput(ENTER, NICK, EMPTY, input);
+}
+std::string Contact::getNickname(void) const { return _nickname; }
+
+//--------------------------------------------------------------> Contact Phone
+void Contact::setPhoneNumber(std::string input) {
+  _phoneNumber = getNumber(ENTER, PHONE, EMPTY, input);
+}
+std::string Contact::getPhoneNumber(void) const { return _phoneNumber; }
+
+//-------------------------------------------------------------> Contact Secret
+void Contact::setDarkestSecret(std::string input) {
+  _darkestSecret = getInput(ENTER, SECRET, EMPTY, input);
+}
+std::string Contact::getDarkestSecret(void) const { return _darkestSecret; }
