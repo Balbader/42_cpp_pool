38ad45736e28939efd8eeb7a71ffed044fcf314e
Basil ALBADER <baalbade@student.42.fr>
2023-11-13 15:26:41 +0100

    WIP

--- ex01/inc/ContactClass.hpp
+++ ex01/inc/ContactClass.hpp
@@ -13,28 +13,22 @@ public:
   ~Contact(void);
 
   void getContactInfo(void);
-
   void printContactInfo(Contact, int);
-
-  void setFirstName(std::string);
-  std::string getFirstName(void) const;
-
-  void setLastName(std::string);
-  std::string getLastName(void) const;
-
-  void setNickname(std::string);
-  std::string getNickname(void) const;
-
-  void setPhoneNumber(std::string);
-  std::string getPhoneNumber(void) const;
-
-  void setDarkestSecret(std::string);
-  std::string getDarkestSecret(void) const;
-
   std::string getInput(std::string, std::string, std::string, std::string);
-
   int checkNumber(std::string input);
   std::string getNumber(std::string, std::string, std::string, std::string);
+
+  void setFirstName(std::string);
+  void setLastName(std::string);
+  void setNickname(std::string);
+  void setPhoneNumber(std::string);
+  void setDarkestSecret(std::string);
+
+  std::string getFirstName(void) const;
+  std::string getLastName(void) const;
+  std::string getNickname(void) const;
+  std::string getPhoneNumber(void) const;
+  std::string getDarkestSecret(void) const;
 
 private:
   std::string _firstName;
