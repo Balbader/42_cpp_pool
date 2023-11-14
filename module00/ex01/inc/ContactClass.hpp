eeec3236d614ea8ef517e1182c405c4100997b42
Basil ALBADER <baalbade@student.42.fr>
2023-11-14 12:19:56 +0100

    WIP

--- module00/ex01/inc/ContactClass.hpp
+++ module00/ex01/inc/ContactClass.hpp
@@ -6,40 +6,45 @@
 #include <iostream>
 #include <string>
 
-class Contact {
+class Contact
+{
 
-public:
-  Contact(void);
-  ~Contact(void);
+    public:
 
-  std::string getInput(std::string, std::string, std::string, std::string);
-  int checkNumber(std::string input);
-  std::string getNumber(std::string, std::string, std::string, std::string);
-  void getContactInfo(void);
-  void printContactInfo(Contact, int);
-  void printExit(void);
-  void printDigitErrorMessage(void);
-  void printCharErrorMessage(void);
+        Contact(void);
+        ~Contact(void);
 
-  void setFirstName(std::string);
-  void setLastName(std::string);
-  void setNickname(std::string);
-  void setPhoneNumber(std::string);
-  void setDarkestSecret(std::string);
+        std::string getInput(std::string, std::string, std::string, std::string);
+        std::string getNumber(std::string, std::string, std::string, std::string);
 
-  std::string getFirstName(void) const;
-  std::string getLastName(void) const;
-  std::string getNickname(void) const;
-  std::string getPhoneNumber(void) const;
-  std::string getDarkestSecret(void) const;
+        int checkNumber(std::string input);
 
-private:
-  std::string _firstName;
-  std::string _lastName;
-  std::string _nickname;
-  std::string _phoneNumber;
-  std::string _darkestSecret;
-  std::string _index;
+        void getContactInfo(void);
+        void printContactInfo(Contact, int);
+        void printExit(void);
+        void printDigitErrorMessage(void);
+        void printCharErrorMessage(void);
+
+        void setFirstName(std::string);
+        void setLastName(std::string);
+        void setNickname(std::string);
+        void setPhoneNumber(std::string);
+        void setDarkestSecret(std::string);
+
+        std::string getFirstName(void) const;
+        std::string getLastName(void) const;
+        std::string getNickname(void) const;
+        std::string getPhoneNumber(void) const;
+        std::string getDarkestSecret(void) const;
+
+    private:
+
+        std::string _firstName;
+        std::string _lastName;
+        std::string _nickname;
+        std::string _phoneNumber;
+        std::string _darkestSecret;
+        std::string _index;
 };
 
 #endif // !CONTACTCLASS_H
