a874a462d2253382b80f9e3d6926e13d69e2d206
Basil ALBADER <baalbade@student.42.fr>
2023-11-16 15:46:42 +0100

    WIP

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -14,17 +14,23 @@
 
 int main(int ac, char **av)
 {
-	if (ac == 1) {
-    std::cout << "* LOUD SND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	if (ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	for (int j = 1; j < ac; ++j) {
-    std::string input = av[j];
-		for (int i = 0; input[i]; ++i) {
+
+	for (int j = 1; j < ac; ++j)
+    {
+        std::string input = av[j];
+
+		for (int i = 0; input[i]; ++i)
+        {
 			input[i] = toupper(input[i]);
 		}
-    std::cout << input;
+        std::cout << input;
 	}
-  std::cout << std::endl;
+
+    std::cout << std::endl;
 	return (0);
 }
