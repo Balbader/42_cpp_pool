c34a638e83b00fae09b17411f1ca72acf108a7d4
Basil ALBADER <baalbade@student.42.fr>
2023-11-16 17:54:31 +0100

    ZombieClass.hpp

---
+++ module01/ex00/inc/ZombieClass.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ZombieClass.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 17:43:20 by baalbade          #+#    #+#             */
+/*   Updated: 2023/11/16 17:43:23 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIECLASS_HPP
+#define ZOMBIECLASS_HPP
+
+#include <iostream>
+
+class ZOMBIE
+{
+
+public:
+		void announce(void);
+		
+
+private:
+		std::string _name;
+
+};
+
+#endif // !ZOMBIECLASS_HPP
