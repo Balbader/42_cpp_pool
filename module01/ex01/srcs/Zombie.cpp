30dd752e8b77ae885ac8513963dad0a2228d362d
Basil ALBADER <baalbade@student.42.fr>
2023-11-17 11:59:26 +0100

    WIP

--- module01/ex01/srcs/Zombie.cpp
+++ module01/ex01/srcs/Zombie.cpp
@@ -10,4 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ZombieClass.hpp"
 
+// ---------------------------------------------------------------> Constructor
+Zombie::Zombie() : _name("No Name")
+{
+    if (DEBUG)
+        std::cout << YELLOW << "Zombie Constructor Called : " << _name << RESET << std::endl;
+}
+
+Zombie::Zombie(std::string name) : _name(name)
+{
+    if (DEBUG)
+        std::cout << LGREEN << "Zombie Constructor Called : " << name << RESET << std::endl;
+}
+
+Zombie::~Zombie()
+{
+    if (DEBUG)
+        std::cout << RED << "Zombie Deconstructor Called : " << _name << RESET << std::endl;
+}
+
+
+// ------------------------------------------------------------------> Announce
+void Zombie::announce(void)
+{
+	std::cout << getName() << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
