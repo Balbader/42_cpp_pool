10ad6aca8f42c92bad8123f88a81ff87dbf75a91
balbader <basilalbapro@gmail.com>
2023-11-18 13:16:07 +0100

    WIP

--- module01/ex03/srcs/Weapon.cpp
+++ module01/ex03/srcs/Weapon.cpp
@@ -11,14 +11,37 @@
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
+#include "colors.hpp"
 
 // ---------------------------------------------------------------> Constructor
-Weapon::Weapon() {
+Weapon::Weapon()
+{
+    if (DEBUG)
+        std::cout << GREEN << "Weapon instance created." << RESET << std::endl;
 }
 
-Weapon::~Weapon() {
+Weapon::Weapon(std::string weaponType)
+{
+    if (DEBUG)
+        std::cout << GREEN << "Weapon instance created." << RESET << std::endl;
 }
+
+
+// ---------------------------------------------------------------> Destructor
+Weapon::~Weapon()
+{
+    if (DEBUG)
+        std::cout << RED << "Weapon instance destroyed." << RESET << std::endl;
+}
+
 
 // -----------------------------------------------------------> Setter & Getter
-// void setType(std::string);
-// std::string getType(void) const;
+void Weapon::setType(std::string weaponType)
+{
+    _type = weaponType;
+}
+
+std::string Weapon::getType(void) const
+{
+    return _type;
+}
