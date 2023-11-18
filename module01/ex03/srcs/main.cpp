c13d334b8b998b2f0ae3fcb9d13b5950b822be85
balbader <basilalbapro@gmail.com>
2023-11-18 13:16:08 +0100

    WIP

--- module01/ex03/srcs/main.cpp
+++ module01/ex03/srcs/main.cpp
@@ -16,14 +16,14 @@
 
 int main(void)
 {
-    // {
-    //     Weapon club = Weapon("crude spiked club");
+    {
+        Weapon club = Weapon("crude spiked club");
 
-    //     HumanA bob("Bob", club);
-    //     bob.attack();
-    //     club.setType("some other type of club");
-    //     bob.attack();
-    // }
+        // HumanA bob("Bob", club);
+        // bob.attack();
+        // club.setType("some other type of club");
+        // bob.attack();
+    }
     // {
     //     Weapon club = Weapon("crude spiked club");
 
