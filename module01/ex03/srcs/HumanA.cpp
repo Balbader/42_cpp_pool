ab2593b1969e5752c9403d47bd25401e0cbabb9f
Basil ALBADER <baalbade@student.42.fr>
2023-11-21 11:09:02 +0100

    WIP

--- module01/ex03/srcs/HumanA.cpp
+++ module01/ex03/srcs/HumanA.cpp
@@ -10,3 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanA.hpp"
+
+// Constructor
+HumanA::HumanA(std::string name, Weapon newWeapon)
+{
+    this->_name = name;
+
+    this->_weaponA = newWeapon;
+
+    if (DEBUG)
+        std::cout << "Human A " << GREEN << this->_name << RESET << " created." << std::endl;
+}
+
+
+// Destructor
+HumanA::~HumanA()
+{
+    if (DEBUG)
+        std::cout << "Human A " << RED << this->_name << RESET << " destroyed." << std::endl;
+}
+
+
+// Get Name
+std::string HumanA::getName() const
+{
+    return this->_name;
+}
+
+
+// Get WeaponA
+std::string HumanA::getWeaponA() const
+{
+    return this->_weaponA.getType();
+}
+
+
+// attack
+void HumanA::attack(std::string name, std::string weapon)
+{
+    std::cout << name << " attacks with their " << weapon << std::endl;
+}
