90a599d47e59d73de18aa6cf57f862864628a841
Basil ALBADER <baalbade@student.42.fr>
2023-11-21 14:00:31 +0100

    WIP

--- module01/ex04/srcs/main.cpp
+++ module01/ex04/srcs/main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <fstream>
 
-int main(void)
+int main(int ac, char **av)
 {
+    if (ac == 3)
+    {
 
-    std::ifstream ifs("numbers");
-    unsigned int dst = 1;
-    unsigned int dst2 = 2;
+    }
 
-    ifs >> dst >> dst2;
-    std::cout << dst << " " << dst2 << std::endl;
-    ifs.close();
-
-    std::ofstream ofs("test.out");
-    ofs << "Hello World" << std::endl;
-    ofs.close();
     return 0;
 }
+
+/*
+
+std::ifstream ifs("numbers");
+unsigned int dst = 1;
+unsigned int dst2 = 2;
+
+ifs >> dst >> dst2;
+std::cout << dst << " " << dst2 << std::endl;
+ifs.close();
+
+std::ofstream ofs("test.out");
+ofs << "Hello World" << std::endl;
+ofs.close();
+
+*/
