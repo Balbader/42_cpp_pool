b3b702f1e9f562c656bb6e8b4a9ffe86b97ebd11
Basil ALBADER <baalbade@student.42.fr>
2023-11-22 10:00:10 +0100

    WIP

--- module01/ex04/srcs/main.cpp
+++ module01/ex04/srcs/main.cpp
@@ -6,11 +6,13 @@ std::string checkWords(std::string line, std::string wordToCheck)
 {
     std::string word;
     int i = 0;
+
     while (line[i] != ' ')
     {
-        std::getline(std::cin, word);
+        std::cin >> word;
         ++i;
     }
+
     return word;
 }
 
@@ -23,21 +25,22 @@ int main(int ac, char **av)
         std::ifstream ifs;
         std::ofstream ofs;
 
-        // ifs.open("hello.txt");
-        ifs.open("test.txt");
-        // ofs.open("test.replace");
+        ifs.open("hello.txt");
+        // ifs.open("test.txt");
+        ofs.open("test.replace");
 
         if (ifs.is_open())
         {
             while (std::getline(ifs, line))
             {
-               // if (ofs.is_open())
-               //  {
-               //      if (line.compare(av[1]) == 0)
-               //      {
-               //          ofs << av[2] << std::endl;
-               //      }
-               //  }
+                std::cout << line << std::endl;
+                std::cout << wordToCheck << std::endl;
+                wordToCheck = checkWords(line, wordToCheck);
+               if (ofs.is_open())
+                {
+                    if (line.compare(av[1]) == 0)
+                        ofs << av[2] << std::endl;
+                }
             }
             ifs.close();
             ofs.close();
