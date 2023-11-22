1cee16adbc371c09079034490e045a282bd5178f
Basil ALBADER <baalbade@student.42.fr>
2023-11-22 14:02:06 +0100

    WIP

--- module01/ex04/srcs/main.cpp
+++ module01/ex04/srcs/main.cpp
@@ -13,9 +13,11 @@ int checkAC(int ac)
         std::cout << "1. File Name" << std::endl;
         std::cout << "2. String #1" << std::endl;
         std::cout << "3. String #2" << std::endl;
-        std::cout << std::endl << YELLOW << "Ex: ./main file.txt \"Hello\" \"World\"" << RESET << std::endl;
+        std::cout << std::endl << YELLOW << "Ex: ./sed file.txt \"Search\" \"Replace\"" << RESET << std::endl;
+
         return 1;
     }
+
     return 0;
 }
 
@@ -26,53 +28,87 @@ int checkAC(int ac)
 int main(int ac, char **av)
 {
     if (!checkAC(ac))
-        return 0;
+        return -1;
 
     else if (ac == 4)
     {
         std::string line;
         std::ifstream ifs;
-        std::ofstream ofs;
 
-    }
+        ifs.open(av[1]);
+
+        if (ifs.is_open())
+        {
+            while (!ifs.eof())
+                line.push_back(ifs.get());
+            line.pop_back();
+            std::cout << GREEN << line << RESET << std::endl; // to delete
+        }
+
+        else
+        {
+            std::cout << RED << "File can't be opened : " << RESET <<  av[1] << std::endl;
+            return (-1);
+        }
+        ifs.close();
+
+        // Find and replace
+        std::string find(av[2]);
+        std::string replace(av[3]);
+        int pos;
+
+        while ((pos = line.find(find)) != std::string::npos)
+        {
+            line.erase(pos, find.length());
+            line.insert(pos, replace);
+        }
+
+        // create and write in file.replace
+        std::string replaceFile (av[1]);
+        replaceFile.insert(replaceFile.length(), ".replace");
+
+        std::ofstream ofs(replace);
+
+
+        if (ofs.is_open())
+            ofs << line;
+
+        else
+        {
+            std::cout << "Unable to create " << av[1] << std::endl;
+            return (1);
+        }
+        
+        std::cout << "\e[38;5;118mEnd of mySed, search and replace program\e[0m" << std::endl;
+        }
 
     return 0;
 }
 
 /*
-if (ac == 3)
-{
-    std::string wordToCheck;
-    std::string line;
-    std::ifstream ifs;
-    std::ofstream ofs;
+            std::string wordToCheck;
+            std::string line;
+            std::ifstream ifs;
+            std::ofstream ofs;
 
-    ifs.open("hello.txt");
-    // ifs.open("test.txt");
-    ofs.open("test.replace");
+            ifs.open("hello.txt");
+            // ifs.open("test.txt");
+            ofs.open("test.replace");
 
-    if (ifs.is_open())
-    {
-        while (std::getline(ifs, line))
-        {
-            std::cout << line << std::endl;
-            std::cout << wordToCheck << std::endl;
-            wordToCheck = checkWords(line, wordToCheck);
-           if (ofs.is_open())
+            if (ifs.is_open())
             {
-                if (line.compare(av[1]) == 0)
-                    ofs << av[2] << std::endl;
+                while (std::getline(ifs, line))
+                {
+                    std::cout << line << std::endl;
+                    std::cout << wordToCheck << std::endl;
+                    wordToCheck = checkWords(line, wordToCheck);
+                   if (ofs.is_open())
+                    {
+                        if (line.compare(av[1]) == 0)
+                            ofs << av[2] << std::endl;
+                    }
+                }
+                ifs.close();
+                ofs.close();
             }
-        }
-        ifs.close();
-        ofs.close();
-    }
-}
-else 
-    std::cout << "You Fucked" << std::endl;
-
-// std::ofstream ofs("test.replace");
-
-// ofs << "Hello world" << std::endl;
-// ofs.close();
 */
