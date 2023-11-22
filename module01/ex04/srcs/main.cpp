30bdfe41b4baeb61496c2972d82bf12d0ac4382f
Basil ALBADER <baalbade@student.42.fr>
2023-11-22 13:00:27 +0100

    WIP

--- module01/ex04/srcs/main.cpp
+++ module01/ex04/srcs/main.cpp
@@ -1,58 +1,78 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
+#include "../inc/colors.hpp"
 
-std::string checkWords(std::string line, std::string wordToCheck)
+int checkAC(int ac)
 {
-    std::string word;
-    int i = 0;
-
-    while (line[i] != ' ')
+    if (ac != 4)
     {
-        std::cin >> word;
-        ++i;
+        std::cout << std::endl << RED << "Wrong Input!" << RESET << std::endl;
+        std::cout << std::endl << "Please make sure to provide 3 arguments for program execution." << std::endl;
+        std::cout << "1. File Name" << std::endl;
+        std::cout << "2. String #1" << std::endl;
+        std::cout << "3. String #2" << std::endl;
+        std::cout << std::endl << YELLOW << "Ex: ./main file.txt \"Hello\" \"World\"" << RESET << std::endl;
+        return 1;
     }
-
-    return word;
+    return 0;
 }
+
+// std::string findAndReplace(std::string line, std::string toFind, std::string replaceWith)
+// {
+// }
 
 int main(int ac, char **av)
 {
-    if (ac == 3)
+    if (!checkAC(ac))
+        return 0;
+
+    else if (ac == 4)
     {
-        std::string wordToCheck;
         std::string line;
         std::ifstream ifs;
         std::ofstream ofs;
 
-        ifs.open("hello.txt");
-        // ifs.open("test.txt");
-        ofs.open("test.replace");
-
-        if (ifs.is_open())
-        {
-            while (std::getline(ifs, line))
-            {
-                std::cout << line << std::endl;
-                std::cout << wordToCheck << std::endl;
-                wordToCheck = checkWords(line, wordToCheck);
-               if (ofs.is_open())
-                {
-                    if (line.compare(av[1]) == 0)
-                        ofs << av[2] << std::endl;
-                }
-            }
-            ifs.close();
-            ofs.close();
-        }
     }
-    else 
-        std::cout << "You Fucked" << std::endl;
-
-    // std::ofstream ofs("test.replace");
-
-    // ofs << "Hello world" << std::endl;
-    // ofs.close();
 
     return 0;
 }
+
+/*
+if (ac == 3)
+{
+    std::string wordToCheck;
+    std::string line;
+    std::ifstream ifs;
+    std::ofstream ofs;
+
+    ifs.open("hello.txt");
+    // ifs.open("test.txt");
+    ofs.open("test.replace");
+
+    if (ifs.is_open())
+    {
+        while (std::getline(ifs, line))
+        {
+            std::cout << line << std::endl;
+            std::cout << wordToCheck << std::endl;
+            wordToCheck = checkWords(line, wordToCheck);
+           if (ofs.is_open())
+            {
+                if (line.compare(av[1]) == 0)
+                    ofs << av[2] << std::endl;
+            }
+        }
+        ifs.close();
+        ofs.close();
+    }
+}
+else 
+    std::cout << "You Fucked" << std::endl;
+
+// std::ofstream ofs("test.replace");
+
+// ofs << "Hello world" << std::endl;
+// ofs.close();
+*/
