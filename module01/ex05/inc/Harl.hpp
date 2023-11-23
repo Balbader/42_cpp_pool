0c3548ee061bd63705420f9baa187909fd93e76a
Basil ALBADER <baalbade@student.42.fr>
2023-11-23 08:42:14 +0100

    Harl.cpp

---
+++ module01/ex05/inc/Harl.hpp
@@ -0,0 +1,32 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+#include "colors.hpp"
+
+#define TOGGLE 1
+
+class Harl
+{
+
+    public:
+
+        Harl();
+        ~Harl();
+
+        void complain(std::string);
+
+    private:
+
+        std::string _complain;
+
+        void _debug(void);
+        void _info(void);
+        void _warning(void);
+        void _error(void);
+        int _checkComplain(std::string);
+
+
+};
+#endif // !HARL_HPP
