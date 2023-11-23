21a7928ea2f0a0afa504b9a1ab416976bd56311e
Basil ALBADER <baalbade@student.42.fr>
2023-11-23 09:12:15 +0100

    WIP

--- module01/ex05/srcs/main.cpp
+++ module01/ex05/srcs/main.cpp
@@ -1,9 +1,13 @@
-#include "Harl.cpp"
+#include "Harl.hpp"
 
 int main(void)
 {
     Harl newComplain;
 
+    newComplain.complain("");
     newComplain.complain("debug");
+    newComplain.complain("info");
+    newComplain.complain("warning");
+    newComplain.complain("error");
     return 0;
 }
