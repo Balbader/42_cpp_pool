5b438b0dae0b51ce177bba15a283e23155664e57
balbader <basilalbapro@gmail.com>
2023-11-30 15:49:53 +0100

    WIP

---
+++ module02/ex02/inc/Fixed.hpp
@@ -0,0 +1,40 @@
+#ifndef FIXED_HPP
+#define FIXED_HP
+
+#include "colors.hpp"
+#include <iostream>
+#include <cmath>
+#include <string>
+
+#define DEBUG 1
+
+class Fixed
+{
+
+	public:
+
+		Fixed();
+		~Fixed();
+
+		Fixed(int const);
+		Fixed(float const);
+
+		Fixed(const Fixed &);
+		Fixed &operator=(const Fixed &);
+
+		int toInt(void) const;
+		float toFloat(void) const;
+
+		void setRawBits(int const);
+		int getRawBits(void) const;
+
+	private:
+
+		int _value;
+		static int const _bits;
+
+};
+
+std::ostream & operator<<(std::ostream & lhs, Fixed const & rhs);
+
+#endif // !FIXED_HPP
