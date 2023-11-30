0f6553ef1e6c66dd26aed7f4e1154cfb4ef7b263
balbader <basilalbapro@gmail.com>
2023-11-30 07:09:50 +0100

    WIP

--- module02/ex00/srcs/main.cpp
+++ module02/ex00/srcs/main.cpp
@@ -1,6 +1,7 @@
 #include <Fixed.hpp>
 
-int main(void) {
+int main(void)
+{
   Fixed a;
   Fixed b(a);
   Fixed c;
@@ -10,5 +11,6 @@ int main(void) {
   std::cout << a.getRawBits() << std::endl;
   std::cout << b.getRawBits() << std::endl;
   std::cout << c.getRawBits() << std::endl;
+
   return 0;
 }
