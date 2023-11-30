9d7a55df9dc21f4d0e93c0c703875e8b2fc848e6
balbader <basilalbapro@gmail.com>
2023-11-30 15:49:55 +0100

    WIP

---
+++ module02/ex02/srcs/Fixed.cpp
@@ -0,0 +1,97 @@
+#include "Fixed.hpp"
+
+int const Fixed::_bits(8);
+
+// ---------------------------------------------------------------- Constructor
+Fixed::Fixed()
+{
+    if (DEBUG)
+        std::cout << LGREEN << "Default constructor called" << RESET << std::endl;
+
+	this->_value = 0;
+}
+
+
+Fixed::Fixed(const Fixed & original)
+{
+    if (DEBUG)
+        std::cout << ORANGE << "Copy constructor called" << RESET << std::endl;
+
+	this->_value = original._value;
+}
+
+
+Fixed::Fixed(int const original)
+{
+	if (DEBUG)
+		std::cout << CYAN << "Int constructor called" << RESET << std::endl;
+
+	this->_value = original * 256;
+}
+
+
+Fixed::Fixed(float const original)
+{
+	if (DEBUG)
+		std::cout << CYAN << "Float constructor called" << RESET << std::endl;
+
+	this->_value = roundf(original * (1 * 256));
+}
+
+
+// ----------------------------------------------------------------- Destructor
+Fixed ::~Fixed()
+{
+    if (DEBUG)
+        std::cout << RED << "Destructor called" << RESET << std::endl;
+}
+
+
+// ------------------------------------------------------------------- Overload
+Fixed & Fixed::operator=(Fixed const & rhs)
+{
+	if (DEBUG)
+		std::cout << YELLOW << "Copy assignment operator called" << RESET << std::endl;
+
+	if (this != &rhs)
+		this->_value = rhs._value;
+
+	return *this;
+}
+
+std::ostream &operator<<(std::ostream &lhs, Fixed const &rhs)
+{
+	lhs << rhs.toFloat();
+
+	return lhs;
+}
+
+
+// ------------------------------------------------------------------ Functions
+int Fixed::toInt(void) const
+{
+	return (this->_value / 256);
+}
+
+float Fixed::toFloat(void) const
+{
+	return ((float)this->_value / 256);
+}
+
+
+// ------------------------------------------------------------ Setter & Getter
+void Fixed::setRawBits(int const bit)
+{
+	if (DEBUG)
+		std::cout << WHITE << "setRawBits() member function called" << RESET << std::endl;
+
+	this->_value = bit;
+}
+
+int Fixed::getRawBits(void) const
+{
+	if (DEBUG)
+		std::cout << BLUE << "getRawBits member function called" << RESET << std::endl;
+
+	return this->_value;
+}
