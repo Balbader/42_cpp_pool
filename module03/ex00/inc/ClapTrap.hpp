9bb6d580e637528ee46547009954ac4708361950
balbader <basilalbapro@gmail.com>
2023-12-03 13:24:31 +0100

    ClapTrap.hpp

---
+++ module03/ex00/inc/ClapTrap.hpp
@@ -0,0 +1,36 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include "colors.hpp"
+
+#include <iostream>
+#include <string>
+
+// #define DEBUG 0
+#define DEBUG 1
+
+class ClapTrap {
+
+public:
+	ClapTrap();
+	~ClapTrap();
+
+	ClapTrap(std::string);
+
+	ClapTrap(const ClapTrap &);
+	ClapTrap &operator=(const ClapTrap &);
+
+	std::string getName();
+	int getPoints();
+	int getEnergyPoints();
+	int getAttackDamage();
+
+private:
+	std::string _name;
+	int _points;
+	int _energyPoints;
+	int _attackDamage;
+	
+};
+
+#endif // !CLAPTRAP_HPP
