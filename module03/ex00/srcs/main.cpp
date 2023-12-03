bef77f5953580d686ff1486971eddbeb1f88a7c5
balbader <basilalbapro@gmail.com>
2023-12-03 14:29:17 +0100

    WIP

--- module03/ex00/srcs/main.cpp
+++ module03/ex00/srcs/main.cpp
@@ -2,18 +2,29 @@
 
 int main(void)
 {
-	ClapTrap OldClap;
-	ClapTrap NewClap("Basil");
+	std::cout << std::endl;
 
+	ClapTrap OldClap;
+
+	std::cout << "Name: " << OldClap.getName() << std::endl;
+	std::cout << "Points: " << OldClap.getPoints() << std::endl;
+	std::cout << "Energy Points: " <<  OldClap.getEnergyPoints() << std::endl;
+	std::cout << std::endl;
+
+	OldClap.setName("Mimi");
 	std::cout << "Name: " << OldClap.getName() << std::endl;
 	std::cout << "Points: " << OldClap.getPoints() << std::endl;
 	std::cout << "Energy Points: " <<  OldClap.getEnergyPoints() << std::endl;
 
 	std::cout << std::endl;
 
+	ClapTrap NewClap("Basil");
+
 	std::cout << "Name: " << NewClap.getName() << std::endl;
 	std::cout << "Points: " << NewClap.getPoints() << std::endl;
 	std::cout << "Energy Points: " <<  NewClap.getEnergyPoints() << std::endl;
+
+	std::cout << std::endl;
 
 	return 0;
 }
