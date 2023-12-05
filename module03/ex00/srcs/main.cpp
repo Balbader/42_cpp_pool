6a4d9c986bcc6c9cb372d1ad67ff1c26eaa2da71
balbader <basilalbapro@gmail.com>
2023-12-05 08:44:22 +0100

    WIP

--- module03/ex00/srcs/main.cpp
+++ module03/ex00/srcs/main.cpp
@@ -8,19 +8,16 @@ int main(void)
 	std::cout << std::endl;
 	std::cout << a;
 	std::cout << b;
-	std::cout << "\n";
+	std::cout << std::endl;
 
 	a.attack("Bob");
-	std::cout << a;
-	std::cout << "\n";
+	std::cout << a << std::endl;
 
 	a.takeDamage(4);
-	std::cout << a;
-	std::cout << "\n";
+	std::cout << a << std::endl;
 
 	a.beRepaired(2);
-	std::cout << a;
-	std::cout << "\n";
+	std::cout << a << std::endl;
 
 	return 0;
 }
