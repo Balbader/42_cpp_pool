86ecf449a7866e5a64ef423f7c99f7672b431b81
balbader <basilalbapro@gmail.com>
2023-12-05 08:42:39 +0100

    WIP

--- module03/ex00/srcs/main.cpp
+++ module03/ex00/srcs/main.cpp
@@ -2,28 +2,25 @@
 
 int main(void)
 {
-	std::cout << std::endl;
-
-	// ClapTrap OldClap;
-	ClapTrap OldClap("Harold");
-
-	std::cout << "Name: " << OldClap.getName() << std::endl;
-	std::cout << "Hit Points: " << OldClap.getHitPoints() << std::endl;
-	std::cout << "Energy Points: " <<  OldClap.getEnergyPoints() << std::endl;
-	std::cout << std::endl;
-
-	OldClap.setName("Mimi");
-	std::cout << "setName(\"Mimi\"): " << OldClap.getName() << std::endl;
+	ClapTrap a("Basil");
+	ClapTrap b(a);
 
 	std::cout << std::endl;
+	std::cout << a;
+	std::cout << b;
+	std::cout << "\n";
 
-	ClapTrap NewClap("Basil");
+	a.attack("Bob");
+	std::cout << a;
+	std::cout << "\n";
 
-	std::cout << "Name: " << NewClap.getName() << std::endl;
-	std::cout << "Hit Points: " << NewClap.getHitPoints() << std::endl;
-	std::cout << "Energy Points: " <<  NewClap.getEnergyPoints() << std::endl;
+	a.takeDamage(4);
+	std::cout << a;
+	std::cout << "\n";
 
-	std::cout << std::endl;
+	a.beRepaired(2);
+	std::cout << a;
+	std::cout << "\n";
 
 	return 0;
 }
