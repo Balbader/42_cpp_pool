3bdaf960d6c773e491e48fdb422fbc01e205a65f
balbader <basilalbapro@gmail.com>
2023-12-05 10:59:18 +0100

    WIP

--- module03/ex01/srcs/main.cpp
+++ module03/ex01/srcs/main.cpp
@@ -1,3 +1,4 @@
+#include "ScavTrap.hpp"
 #include "ClapTrap.hpp"
 
 int main(void)
@@ -5,9 +6,16 @@ int main(void)
 	ClapTrap a("Basil");
 	ClapTrap b(a);
 
+	ScavTrap c;
+
+	c.setName("Mimi");
+
 	std::cout << std::endl;
+
 	std::cout << a;
 	std::cout << b;
+	std::cout << c;
+
 	std::cout << std::endl;
 
 	a.attack("Bob");
