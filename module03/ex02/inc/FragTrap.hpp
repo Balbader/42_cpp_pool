9ccbe5afd807e54adf39af772950cff11bcd3985
balbader <basilalbapro@gmail.com>
2023-12-11 15:32:01 +0100

    WIP

--- module03/ex02/inc/FragTrap.hpp
+++ module03/ex02/inc/FragTrap.hpp
@@ -1,5 +1,15 @@
 #ifndef FRAGTRAP_HPP
 #define FRAGTRAP_HPP
 
+class FragTrap : public ClapTrap {
+public:
+	FragTrap();
+	FragTrap(const FragTrap &);
+	FragTrap &operator=(const FragTrap &);
+	~FragTrap();
+
+private:
+	
+};
 
 #endif // !FRAGTRAP_HPP
