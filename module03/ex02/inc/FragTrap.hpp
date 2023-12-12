d35a71915bc19cd24aaa147b4c602999961ab086
Basil ALBADER <baalbade@student.42.fr>
2023-12-12 07:55:53 +0100

    WIP

--- module03/ex02/inc/FragTrap.hpp
+++ module03/ex02/inc/FragTrap.hpp
@@ -1,15 +1,14 @@
 #ifndef FRAGTRAP_HPP
 #define FRAGTRAP_HPP
 
-#include "ScavTrap.hpp"
+#include "ClapTrap.hpp"
 
 class FragTrap : public ClapTrap {
 public:
 	FragTrap(std::string);
 	~FragTrap();
 
-	FragTrap(const FragTrap &);
-	FragTrap &operator=(const FragTrap &);
+    void highFivesGuys(void);
 };
 
 #endif // !FRAGTRAP_HPP
