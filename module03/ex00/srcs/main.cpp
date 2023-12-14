2352566b94edbc8c1f5020de2d30547f2ce1adad
Basil ALBADER <baalbade@student.42.fr>
2023-12-14 14:50:47 +0100

    WIP

--- module03/ex00/srcs/main.cpp
+++ module03/ex00/srcs/main.cpp
@@ -5,19 +5,16 @@ int main(void)
 	ClapTrap a("Basil");
 	ClapTrap b(a);
 
-	std::cout << std::endl;
-	std::cout << a;
-	std::cout << b;
-	std::cout << std::endl;
+	std::cout << std::endl << a;
+	std::cout << b << std::endl;
 
 	a.attack("Bob");
-	std::cout << a << std::endl;
-
+    std::cout << std::endl;
 	a.takeDamage(4);
-	std::cout << a << std::endl;
-
+    std::cout << std::endl;
 	a.beRepaired(2);
-	std::cout << a << std::endl;
+
+    std::cout << std::endl;
 
 	return 0;
 }
