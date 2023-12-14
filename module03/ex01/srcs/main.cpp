5f9f151cb3ebf0ac3dcb5ab405e94ddc38a2ba9b
Basil ALBADER <baalbade@student.42.fr>
2023-12-14 15:25:43 +0100

    WIP

--- module03/ex01/srcs/main.cpp
+++ module03/ex01/srcs/main.cpp
@@ -9,9 +9,9 @@ int main(void)
 	ScavTrap c("FIFI");
 	ScavTrap d("LOULOU");
 
-	std::cout << LRED << a << RESET;
-	std::cout << LGREEN << b << RESET;
-	std::cout << LBLUE << c << RESET;
+	std::cout << GREEN << a << RESET;
+	std::cout << GREEN << b << RESET;
+	std::cout << YELLOW << c << RESET;
 	std::cout << YELLOW << d << RESET << std::endl;
 
 	a.attack(c.getName());
