b13b584713bb3e0f9ff7a4410778ebf31c518033
Basil ALBADER <baalbade@student.42.fr>
2023-12-15 18:27:10 +0100

    WIP

--- module04/ex03/srcs/AMateria.cpp
+++ module04/ex03/srcs/AMateria.cpp
@@ -1,3 +1,28 @@
 #include "AMateria.hpp"
 
+// ---------------------------------------------------------------- Constructor
+AMateria::AMateria()
+{
+    if (DEBUG)
+        std::cout << "AMateria Base Constructor Called" << std::endl;
+}
+
+AMateria::AMateria(std::string const & type)
+{
+    if (DEBUG)
+        std::cout << "AMateria Base Name Constructor Called" << std::endl;
+
+    this->type_ = type;
+}
+
+// ----------------------------------------------------------------- Destructor
+AMateria::~AMateria()
+{
+    if (DEBUG)
+        std::cout << "AMateria Base Destructor Called" << std::endl;
+}
+
+// ----------------------------------------------------------- Copy Constructor
+
+// ------------------------------------------------------------------- Overload
 
