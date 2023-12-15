1421adaf24740e34004a67ece909c3b4c9aaaafd
Basil ALBADER <baalbade@student.42.fr>
2023-12-15 16:56:48 +0100

    WIP

---
+++ module04/ex03/inc/AMateria.hpp
@@ -0,0 +1,19 @@
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#define DEBUG 1
+
+class AMateria
+{
+
+public:
+    AMateria();
+    AMateria(const AMateria &);
+    AMateria &operator=(const AMateria &);
+    ~AMateria();
+
+private:
+    
+};
+
+#endif // !AMATERIA_HPP
