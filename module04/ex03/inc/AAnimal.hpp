a77d330fdb2d25bcfaa5e2f7b672b95a19008816
Basil ALBADER <baalbade@student.42.fr>
2023-12-15 16:43:09 +0100

    WIP

---
+++ module04/ex03/inc/AAnimal.hpp
@@ -0,0 +1,29 @@
+#ifndef AANIMAL_HPP
+#define AANIMAL_HPP
+
+#include "colors.hpp"
+#include <iostream>
+#include <string>
+
+#define DEBUG 1
+
+class AAnimal {
+
+public:
+	AAnimal();
+	virtual ~AAnimal();
+
+	AAnimal(const AAnimal &);
+	AAnimal &operator=(const AAnimal &);
+
+	virtual void makeSound() const = 0;
+	std::string getType() const;
+
+protected:
+	std::string _type;
+
+private:
+	
+};
+
+#endif // !ANIMAL_HPP
---
+++ module04/ex03/inc/Brain.hpp
@@ -0,0 +1,24 @@
+#ifndef BRAIN_HPP
+#define BRAIN_HPP
+
+#include "AAnimal.hpp"
+
+class Brain {
+
+public:
+	Brain();
+	~Brain();
+
+	Brain(const Brain &);
+	Brain &operator=(const Brain &);
+
+    std::string getIdea(int) const;
+
+protected:
+    std::string ideas[100];
+
+private:
+	
+};
+
+#endif // !BRAIN_HPP
