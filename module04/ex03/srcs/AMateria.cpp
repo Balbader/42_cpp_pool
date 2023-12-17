50d66c156fb690308b7b215a0ea2e5df79a2c019
balbader <basilalbapro@gmail.com>
2023-12-17 10:08:50 +0100

    WIP

--- module04/ex03/srcs/AMateria.cpp
+++ module04/ex03/srcs/AMateria.cpp
@@ -1,49 +1,42 @@
 #include "AMateria.hpp"
 
 // ---------------------------------------------------------------- Constructor
-AMateria::AMateria()
-{
-    if (DEBUG)
-        std::cout << "AMateria Base Constructor Called" << std::endl;
+AMateria::AMateria() {
+  if (DEBUG)
+    std::cout << "AMateria Base Constructor Called" << std::endl;
 }
 
-AMateria::AMateria(std::string const & type)
-{
-    if (DEBUG)
-        std::cout << "AMateria Base Name Constructor Called" << std::endl;
+AMateria::AMateria(std::string const &type) {
+  if (DEBUG)
+    std::cout << "AMateria Base Name Constructor Called" << std::endl;
 
-    this->type_ = type;
+  this->type_ = type;
 }
-
 
 // ----------------------------------------------------------------- Destructor
-AMateria::~AMateria()
-{
-    if (DEBUG)
-        std::cout << "AMateria Base Destructor Called" << std::endl;
+AMateria::~AMateria() {
+  if (DEBUG)
+    std::cout << "AMateria Base Destructor Called" << std::endl;
 }
 
-
 // ----------------------------------------------------------- Copy Constructor
-AMateria::AMateria(const AMateria & rhs)
-{
-    if (DEBUG)
-        std::cout << "AMateria Base Copy Constructor Called" << std::endl;
+AMateria::AMateria(const AMateria &rhs) {
+  if (DEBUG)
+    std::cout << "AMateria Base Copy Constructor Called" << std::endl;
 
-    *this = rhs;
+  *this = rhs;
 }
 
 // ------------------------------------------------------------------- Overload
-AMateria& AMateria::operator=(const AMateria & rhs)
-{
-    if (DEBUG)
-        std::cout << "AMateria Base Assignment Operator Called" << std::endl;
+AMateria &AMateria::operator=(const AMateria &rhs) {
+  if (DEBUG)
+    std::cout << "AMateria Base Assignment Operator Called" << std::endl;
 
-    if (this != &rhs)
-        this->type_ = rhs.type_;
+  if (this != &rhs)
+    this->type_ = rhs.type_;
 
-    return *this;
+  return *this;
 }
 
 // --------------------------------------------------------------------- Getter
-std::string const & AMateria::getType() const { return this->type_; }
+std::string const &AMateria::getType() const { return this->type_; }
