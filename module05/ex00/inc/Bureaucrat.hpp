a801aec6ab03782bcc28e1a4b64c91cdaf3a5c99
balbader <basilalbapro@gmail.com>
2023-12-24 15:08:10 +0100

    WIP

--- module05/ex00/inc/Bureaucrat.hpp
+++ module05/ex00/inc/Bureaucrat.hpp
@@ -40,6 +40,7 @@ public:
   void incrementGrade();
   void decrementGrade();
   void printGrade();
+  int gradeOutOfRange(int);
 
   // ------------------------------------------------------------------ Setters
   void setName(std::string);
