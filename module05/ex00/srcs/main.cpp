82e8584bf949efa4ed676594bf35fed140c7a94d
balbader <basilalbapro@gmail.com>
2023-12-24 10:22:59 +0100

    WIP

--- module05/ex00/srcs/main.cpp
+++ module05/ex00/srcs/main.cpp
@@ -1,28 +1,32 @@
 #include "Bureaucrat.hpp"
 
 int main() {
+
   std::cout << std::endl;
 
-  // Bureaucrat mimi("mimi", -22);
   Bureaucrat faten("faten", 133);
-  std::cout << faten << "\n";
+  std::cout << "\n" << faten << "\n";
 
+  // ==========================================================================
   faten.incrementGrade(10);
-  std::cout << LYELLOW
-            << "new grade after incrementGrade(10) : " << faten.getGrade()
-            << RESET << '\n';
+  std::cout << "new grade after incrementGrade(10) : " << LYELLOW
+            << faten.getGrade() << RESET << '\n';
 
+  // ==========================================================================
   faten.decrementGrade(5);
   std::cout << "new grade after decrementGrade(5) : " << ORANGE
             << faten.getGrade() << RESET << "\n";
-  faten.decrementGrade(10);
-  std::cout << "new grade after decrementGrade(10) : " << ORANGE
-            << faten.getGrade() << RESET << "\n\n";
 
-  // std::cout << basil << "\n";
+  // ==========================================================================
+  // faten.decrementGrade(10);
+  // std::cout << "new grade after decrementGrade(10) : " << ORANGE
+  //           << faten.getGrade() << RESET << "\n\n";
+
+  // Bureaucrat mimi("mimi", -22);
   // std::cout << mimi << "\n";
-  std::cout << faten << "\n";
+  // std::cout << MAGENTA << faten << "\n" << RESET;
 
   // Bureaucrat basil("Basil", 333);
+  // std::cout << basil << "\n";
   return 0;
 }
