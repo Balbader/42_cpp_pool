2c1b84c8ce85a3f94e4b4be2ec9207128e49655b
balbader <basilalbapro@gmail.com>
2023-12-24 16:54:11 +0100

    WIP

--- module05/ex00/srcs/main.cpp
+++ module05/ex00/srcs/main.cpp
@@ -7,14 +7,14 @@ int main() {
   Bureaucrat faten("faten", 133);
   std::cout << "\n" << faten << "\n";
 
-  faten.incrementGrade();
-  std::cout << "\n" << faten << "\n";
+  // faten.incrementGrade();
+  // std::cout << "\n" << faten << "\n";
 
-  faten.decrementGrade();
-  std::cout << "\n" << faten << "\n";
+  // faten.decrementGrade();
+  // std::cout << "\n" << faten << "\n";
 
   Bureaucrat mimi("mimi", 331);
-  std::cout << "\n" << mimi << "\n";
+  // std::cout << "\n" << mimi << "\n";
 
   return 0;
 }
