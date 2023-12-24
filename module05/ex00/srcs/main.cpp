7b40ffea824a4ec1c547879ff1dddd4e2bb27b92
balbader <basilalbapro@gmail.com>
2023-12-24 09:43:22 +0100

    WIP

--- module05/ex00/srcs/main.cpp
+++ module05/ex00/srcs/main.cpp
@@ -3,22 +3,26 @@
 int main() {
   std::cout << std::endl;
 
-  Bureaucrat basil("Basil", 333);
-  Bureaucrat mimi("mimi", -22);
+  // Bureaucrat mimi("mimi", -22);
   Bureaucrat faten("faten", 133);
+  std::cout << faten << "\n";
 
-  faten.incrementGrade();
+  faten.incrementGrade(10);
   std::cout << LYELLOW
-            << "new grade after incrementGrade(1) : " << faten.getGrade()
+            << "new grade after incrementGrade(10) : " << faten.getGrade()
             << RESET << '\n';
 
-  faten.decrementGrade();
-  faten.decrementGrade();
-  std::cout << "new grade after decrementGrade(2) : " << ORANGE
+  faten.decrementGrade(5);
+  std::cout << "new grade after decrementGrade(5) : " << ORANGE
+            << faten.getGrade() << RESET << "\n";
+  faten.decrementGrade(10);
+  std::cout << "new grade after decrementGrade(10) : " << ORANGE
             << faten.getGrade() << RESET << "\n\n";
 
-  std::cout << basil << "\n";
-  std::cout << mimi << "\n";
+  // std::cout << basil << "\n";
+  // std::cout << mimi << "\n";
   std::cout << faten << "\n";
+
+  // Bureaucrat basil("Basil", 333);
   return 0;
 }
