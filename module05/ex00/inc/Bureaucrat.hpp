adb2349427612b5cb0196d1f17b318b527bb2b6b
Basil ALBADER <baalbade@student.42.fr>
2023-12-26 12:19:37 +0100

    WIP

--- module05/ex00/inc/Bureaucrat.hpp
+++ module05/ex00/inc/Bureaucrat.hpp
@@ -10,67 +10,62 @@
 
 #define DEBUG 1
 
+// --------------------------------------------------------------- Exceptions
+class GradeTooHighException : public std::exception {
+public:
+  GradeTooHighException(const std::string &msg, int grade)
+      : message_(msg), wrongGrade_(grade) {
+    try {
+      if (this->wrongGrade_ < 1)
+        throw std::exception();
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << "\n";
+    }
+  }
+  virtual const char *what() const throw() { return message_.c_str(); }
+  virtual ~GradeTooHighException() throw() {}
+
+private:
+  std::string message_;
+  int wrongGrade_;
+};
+
+class GradeTooLowException : public std::exception {
+public:
+  GradeTooLowException(const std::string &msg, int grade)
+      : message_(msg), wrongGrade_(grade) {
+    try {
+      if (this->wrongGrade_ > 150)
+        throw std::exception();
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << "\n";
+    }
+  }
+  virtual const char *what() const throw() { return message_.c_str(); }
+  virtual ~GradeTooLowException() throw() {}
+
+private:
+  std::string message_;
+  int wrongGrade_;
+};
+
 class Bureaucrat {
 
 public:
-  // ------------------------------------------------- Constructor & Destructor
   Bureaucrat(std::string, int);
   ~Bureaucrat();
 
-  // ----------------------------------- Copy constructor & Assignment operator
   Bureaucrat(const Bureaucrat &);
   Bureaucrat &operator=(const Bureaucrat &);
 
-  // --------------------------------------------------------------- Exceptions
-  class GradeTooHighException : public std::exception {
-  public:
-    GradeTooHighException(const std::string &msg, int grade)
-        : message_(msg), wrongGrade_(grade) {
-      try {
-        if (this->wrongGrade_ < 1)
-          throw std::exception();
-      } catch (const std::exception &e) {
-        std::cerr << e.what() << "\n";
-      }
-    }
-    virtual const char *what() const throw() { return message_.c_str(); }
-    virtual ~GradeTooHighException() throw() {}
-
-  private:
-    std::string message_;
-    int wrongGrade_;
-  };
-
-  class GradeTooLowException : public std::exception {
-  public:
-    GradeTooLowException(const std::string &msg, int grade)
-        : message_(msg), wrongGrade_(grade) {
-      try {
-        if (this->wrongGrade_ > 150)
-          throw std::exception();
-      } catch (const std::exception &e) {
-        std::cerr << e.what() << "\n";
-      }
-    }
-    virtual const char *what() const throw() { return message_.c_str(); }
-    virtual ~GradeTooLowException() throw() {}
-
-  private:
-    std::string message_;
-    int wrongGrade_;
-  };
-
-  // ------------------------------------------------------------------ Methods
   void incrementGrade();
   void decrementGrade();
   void printGrade();
   void isGradeOutOfRange(int);
 
-  // ------------------------------------------------------------------ Setters
   void setName(std::string);
   void setGrade(int);
 
-  // ------------------------------------------------------------------ Getters
   const std::string &getName() const;
   unsigned int const &getGrade() const;
 
