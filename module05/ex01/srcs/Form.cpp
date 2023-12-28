a0b1cd66f6f15e7c097cab7370b488ea3e6fe738
Basil ALBADER <baalbade@student.42.fr>
2023-12-28 08:23:03 +0100

    WIP

--- module05/ex01/srcs/Form.cpp
+++ module05/ex01/srcs/Form.cpp
@@ -3,16 +3,16 @@
 // ---------------------------------------------------------------- Constructor
 Form::Form() {
   if (DEBUG)
-    std::cout << LYELLOW << "Form base constructor called" << RESET << "\n";
+    std::cout << YELLOW << "Form base constructor called" << RESET << "\n";
 }
 
 Form::Form(std::string name, int grade) : gradeToSign_(grade), name_(name) {
   if (DEBUG)
-    std::cout << LYELLOW << "Form base with args constructor called" << RESET << "\n";
+    std::cout << YELLOW << "Form base with args constructor called" << RESET << "\n";
 }
 
 // ---------------------------------------------------------------- Destructor
 Form::~Form() {
   if (DEBUG)
-    std::cout << LYELLOW << "Form base destructor called" << RESET << "\n";
+    std::cout << YELLOW << "Form base destructor called" << RESET << "\n";
 }
