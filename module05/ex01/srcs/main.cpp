123a80eae03f8ec8c9bbb8bdd6073cf74c953ffd
balbader <basilalbapro@gmail.com>
2023-12-30 06:57:02 +0100

    WIP

--- module05/ex01/srcs/main.cpp
+++ module05/ex01/srcs/main.cpp
@@ -1,56 +1,20 @@
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
 
-int main() {
-  std::cout << "\n";
+int main(int ac, char **av) {
 
-  Bureaucrat ceo("Mimi", 448);
-  Bureaucrat coo("Riri", -31);
-  Bureaucrat cfo("Fifi", 42);
+  if (ac != 4) {
+    std::cerr << RED << "ERROR OCURED!\n" << RESET;
+    std::cerr << "Please provide the following arguments in the folllowing "
+                 "order to successfully execute the program: \n"
+              << "1. Name of executable: " << LGREEN << "<./Bureaucrat\n"
+              << RESET
+              << "2. Grade to execute (from 1 to 150 included): " << LGREEN
+              << "148\n"
+              << RESET
+              << "3. Grade to sign (from 1 to 150 included): " << LGREEN
+              << " 42" << RESET << std::endl;
+  }
 
-  std::cout << cfo << "\n";
-  std::cout << coo;
-  std::cout << ceo;
-  std::cout << "\n";
-
-  Form transfer("transfer", 144, 42);
-  std::cout << transfer;
-  std::cout << "\n";
-
-  Form profile("Doudou", -420, 33);
-  std::cout << profile;
-  std::cout << "\n";
   return 0;
 }
-
-// void	createForm(std::string name, int requiredGradeToBeExecuted, int requiredGradeToBeSigned)
-// {
-// 	Bureaucrat Gurvan;
-// 	Form	chess_form;
-
-// 	std ::cout << chess_form;
-// 	chess_form.beSigned(Gurvan);
-// 	std::cout << "\n\n\n";
-// 	Gurvan.signForm(chess_form);
-
-// 	Form	random_form(name, requiredGradeToBeExecuted, requiredGradeToBeSigned);
-
-// 	std ::cout << random_form;
-// 	random_form.beSigned(Gurvan);
-
-// }
-
-// int	main(int argc, char **argv)
-// {
-// 	if (argc != 4) {
-// 		std::cerr << "Error: usage is ./bureaucrat <name> <required grade to be executed> <required grade to be signed>.\n";
-// 		return (ERROR);
-// 	}
-
-// 	try
-// 		createForm(argv[1], atoi(argv[2]), atoi(argv[3]));
-// 	catch (std::exception)
-// 		return (ERROR);
-
-// 	return (SUCCESS);
-// }
