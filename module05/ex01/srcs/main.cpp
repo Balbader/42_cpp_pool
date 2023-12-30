3efac5d5fa6abcae53a1a73aed393ff7300f5c89
balbader <basilalbapro@gmail.com>
2023-12-30 05:05:29 +0100

    WIP

--- module05/ex01/srcs/main.cpp
+++ module05/ex01/srcs/main.cpp
@@ -22,3 +22,35 @@ int main() {
   std::cout << "\n";
   return 0;
 }
+
+// void	createForm(std::string name, int requiredGradeToBeExecuted, int requiredGradeToBeSigned)
+// {
+// 	Bureaucrat Gurvan;
+// 	Form	chess_form;
+
+// 	std ::cout << chess_form;
+// 	chess_form.beSigned(Gurvan);
+// 	std::cout << "\n\n\n";
+// 	Gurvan.signForm(chess_form);
+
+// 	Form	random_form(name, requiredGradeToBeExecuted, requiredGradeToBeSigned);
+
+// 	std ::cout << random_form;
+// 	random_form.beSigned(Gurvan);
+
+// }
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc != 4) {
+// 		std::cerr << "Error: usage is ./bureaucrat <name> <required grade to be executed> <required grade to be signed>.\n";
+// 		return (ERROR);
+// 	}
+
+// 	try
+// 		createForm(argv[1], atoi(argv[2]), atoi(argv[3]));
+// 	catch (std::exception)
+// 		return (ERROR);
+
+// 	return (SUCCESS);
+// }
