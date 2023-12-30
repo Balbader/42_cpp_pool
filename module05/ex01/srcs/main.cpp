4fea2eecc34b294ef037f9b1615153b6d4033aa6
balbader <basilalbapro@gmail.com>
2023-12-30 07:07:45 +0100

    WIP

--- module05/ex01/srcs/main.cpp
+++ module05/ex01/srcs/main.cpp
@@ -3,18 +3,26 @@
 
 int main(int ac, char **av) {
 
-  if (ac != 4) {
-    std::cerr << RED << "ERROR OCURED!\n" << RESET;
-    std::cerr << "Please provide the following arguments in the folllowing "
+  if (ac == 3) {
+    Form WireTransfer;
+    Bureaucrat Basil;
+
+    std::cout << WireTransfer;
+    std::cout << Basil;
+  }
+  else
+    std::cerr << RED << "ERROR OCCURED!\n" << RESET
+              << "Please provide the following arguments in the folllowing "
                  "order to successfully execute the program: \n"
-              << "1. Name of executable: " << LGREEN << "<./Bureaucrat\n"
+              << "1. Name of executable: " << LGREEN
+              << "<./Bureaucrat\n"
               << RESET
               << "2. Grade to execute (from 1 to 150 included): " << LGREEN
               << "148\n"
               << RESET
               << "3. Grade to sign (from 1 to 150 included): " << LGREEN
-              << " 42" << RESET << std::endl;
-  }
+              << " 42"
+              << RESET << std::endl;
 
   return 0;
 }
