bba71cff20c8c744e28f7d74bebf33f3383d4a60
balbader <basilalbapro@gmail.com>
2023-12-31 02:57:23 +0100

    WIP

--- module05/ex02/inc/ShrubberyCreationForm.hpp
+++ module05/ex02/inc/ShrubberyCreationForm.hpp
@@ -1,4 +1,15 @@
 #ifndef SHRUBBERYCREATIONFORM_HPP
 #define SHRUBBERYCREATIONFORM_HPP
 
+class ShrubberyCreationForm {
+public:
+  ShrubberyCreationForm();
+  ~ShrubberyCreationForm();
+
+  ShrubberyCreationForm(const ShrubberyCreationForm &);
+  ShrubberyCreationForm &operator=(const ShrubberyCreationForm &);
+
+private:
+};
+
 #endif // !SHRUBBERYCREATIONFORM_HPP
