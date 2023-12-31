7fa93173047dd45eb0b315ba6c8cd45149cd4a38
balbader <basilalbapro@gmail.com>
2023-12-31 09:08:19 +0100

    WIP

--- module05/ex02/inc/PresidentialPardonForm.hpp
+++ module05/ex02/inc/PresidentialPardonForm.hpp
@@ -1,7 +1,7 @@
 #ifndef PRESIDENTIALPARDONFORM_HPP
 #define PRESIDENTIALPARDONFORM_HPP
 
-#include AForm;
+#include "AForm.hpp";
 
 class PresidentialPardonForm : public AForm {
 public:
@@ -10,6 +10,7 @@ public:
   PresidentialPardonForm(std::string const target);
   virtual ~PresidentialPardonForm();
 
+  PresidentialPardonForm(const PresidentialPardonForm &);
   PresidentialPardonForm &operator=(const PresidentialPardonForm &);
 
   virtual void beSigned(Bureaucrat const &bureaucrat);
@@ -17,59 +18,10 @@ public:
 
 private:
   std::string const _target;
-
   PresidentialPardonForm();
-  PresidentialPardonForm (const PresidentialPardonForm &);
+
 };
 
 std::ostream &operator<<(std::ostream &lhs, PresidentialPardonForm const &rhs);
 
 #endif // !PRESIDENTIALPARDONFORM_HPP
-//
-//
-//
-//
-//
-//
-PresidentialPardonForm::PresidentialPardonForm(std::string name, std::string target)
-: AForm(name, 25, 5, target)
-{
-	std::cout << RED << "PresidentialPardonForm constructor called\n" << RESET;
-
-}
-
-PresidentialPardonForm::~PresidentialPardonForm()
-{
-	std::cout << YELLOW << "PresidentialPardonForm destructor called\n" << RESET;
-}
-
-PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& rhs) 
-:AForm(rhs)
-{
-	std::cout << BLUE << "PresidentialPardonForm copy constructor called\n" << RESET;
-	*this = rhs;
-}
-
-PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& rhs)
-{
-	std::cout << GREEN << "PresidentialPardonForm copy assignment called\n" << RESET;
-	if (this != &rhs)
-	{
-		this->setTarget(rhs.getTarget());
-	}
-	return *this;
-}
-
-int PresidentialPardonForm::execute(Bureaucrat const & executor) const
-{
-	if (this->getSignedForm() == false)
-	{
-		throw unsignedFormtoExecuteException(executor, *this);
-	}
-	if (executor.getGrade() > this->getGradeToExecute())
-	{
-		throw GradeTooLowtoExecuteException(executor, *this);
-	}
-	std::cout << this->getTarget() << " has been pardonned by Zaphod Beeblebrox\n";
-	return 0;
-}
