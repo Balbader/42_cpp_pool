c0e5d8f757b7de3ae01f42691ed4d6e314ac9409
balbader <basilalbapro@gmail.com>
2023-12-31 03:47:57 +0100

    WIP

--- module05/ex02/inc/PresidentialPardonForm.hpp
+++ module05/ex02/inc/PresidentialPardonForm.hpp
@@ -1,15 +1,27 @@
 #ifndef PRESIDENTIALPARDONFORM_HPP
 #define PRESIDENTIALPARDONFORM_HPP
 
-class PresidentialPardonForm {
-public:
-  PresidentialPardonForm();
-  ~PresidentialPardonForm();
+#include AForm;
 
-  PresidentialPardonForm(const PresidentialPardonForm &);
+class PresidentialPardonForm : public AForm {
+public:
+  std::string getTarget(void) const;
+
+  PresidentialPardonForm(std::string const target);
+  virtual ~PresidentialPardonForm();
+
   PresidentialPardonForm &operator=(const PresidentialPardonForm &);
 
+  virtual void beSigned(Bureaucrat const &bureaucrat);
+  virtual void execute(Bureaucrat const &executor) const;
+
 private:
+  std::string const _target;
+
+  PresidentialPardonForm();
+  PresidentialPardonForm (const PresidentialPardonForm &);
 };
+
+std::ostream &operator<<(std::ostream &lhs, PresidentialPardonForm const &rhs);
 
 #endif // !PRESIDENTIALPARDONFORM_HPP
