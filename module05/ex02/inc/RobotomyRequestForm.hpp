3b35c23867ac356f6cbecdd5ede44ebf02619bf2
balbader <basilalbapro@gmail.com>
2023-12-31 02:57:22 +0100

    WIP

--- module05/ex02/inc/RobotomyRequestForm.hpp
+++ module05/ex02/inc/RobotomyRequestForm.hpp
@@ -1,4 +1,15 @@
 #ifndef ROBOTOMYREQUESTFORM_HPP
 #define ROBOTOMYREQUESTFORM_HPP
 
+class RobotomyRequestForm {
+public:
+  RobotomyRequestForm();
+  ~RobotomyRequestForm();
+
+  RobotomyRequestForm(const RobotomyRequestForm &);
+  RobotomyRequestForm &operator=(const RobotomyRequestForm &);
+
+private:
+};
+
 #endif // !ROBOTOMYREQUESTFORM_HPP
