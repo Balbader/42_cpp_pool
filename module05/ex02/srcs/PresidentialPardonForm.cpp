f1def1f82dd933eca1abbae925f01bc80c814107
balbader <basilalbapro@gmail.com>
2024-01-01 15:41:25 +0100

    WIP

--- module05/ex02/srcs/PresidentialPardonForm.cpp
+++ module05/ex02/srcs/PresidentialPardonForm.cpp
@@ -3,34 +3,42 @@
 
 // ---------------------------------------------------------------- Constructor
 PresidentialPardonForm::PresidentialPardonForm() {
-  if (DEBUG)
-    std::cout << MAGENTA << "PresidentialPardonForm base constructor called"
-              << RESET << std::endl;
+    if (DEBUG)
+        std::cout << MAGENTA << "PresidentialPardonForm base constructor called"
+                  << RESET << std::endl;
 }
 
 PresidentialPardonForm::PresidentialPardonForm(std::string name, std::string target) {
-  if (DEBUG)
-    std::cout << MAGENTA << "PresidentialPardonForm args derived constructor called"
-              << RESET << std::endl;
+    if (DEBUG)
+        std::cout << MAGENTA << "PresidentialPardonForm args derived constructor called"
+                  << RESET << std::endl;
 }
 
 // ----------------------------------------------------------------- Destructor
 ~PresidentialPardonForm::PresidentialPardonForm() {
-  if (DEBUG)
-    std::cout << MAGENTA << "PresidentialPardonForm derived destructor called"
-              << RESET << std::endl;
+    if (DEBUG)
+        std::cout << MAGENTA << "PresidentialPardonForm derived destructor called"
+                  << RESET << std::endl;
 }
 
 // ----------------------------------------------------------- Copy Constructor
 PresidentialPardonForm::PresidentialPardonForm(const Bureaucrat &rhs) {
-  if (DEBUG)
-    std::cout << MAGENTA << "PresidentialPardonForm derived copy constructor called"
-              << RESET << std::endl;
-  if (*this != rhs)
-
+    if (DEBUG)
+        std::cout << MAGENTA << "PresidentialPardonForm derived copy constructor called"
+                  << RESET << std::endl;
 }
 
 // ------------------------------------------------------------------- Overload
+PresidentialPardonForm & PresidentialPardonForm::operator=(PresidentialPardonForm & rhs) {
+    if (DEBUG)
+        std::cout << MAGENTA << "PresidentialPardonForm assignment copy operator called"
+                  << RESET << std::endl;
+
+    if (*this != rhs)
+        this = rhs;
+
+    return *this;
+}
 
 // -------------------------------------------------------------------- Methods
 
