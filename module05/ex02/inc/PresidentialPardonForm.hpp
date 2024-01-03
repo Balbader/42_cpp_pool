16e1a6bef58046732de974ea6e8f1f732d7027ad
balbader <basilalbapro@gmail.com>
2024-01-03 10:27:53 +0100

    WIP

--- module05/ex02/inc/PresidentialPardonForm.hpp
+++ module05/ex02/inc/PresidentialPardonForm.hpp
@@ -5,21 +5,23 @@
 
 class PresidentialPardonForm : public AForm {
 
-    public:
-        PresidentialPardonForm(std::string const target);
-        virtual ~PresidentialPardonForm();
+public:
+    PresidentialPardonForm(std::string const target);
+    virtual ~PresidentialPardonForm();
 
-        PresidentialPardonForm(const PresidentialPardonForm &);
-        PresidentialPardonForm &operator=(const PresidentialPardonForm &);
+    PresidentialPardonForm(const PresidentialPardonForm &);
+    PresidentialPardonForm &operator=(const PresidentialPardonForm &);
 
-        virtual void beSigned(Bureaucrat const &bureaucrat);
-        virtual void execute(Bureaucrat const &executor) const;
+    virtual void beSigned(Bureaucrat const &bureaucrat);
+    virtual void execute(Bureaucrat const &executor) const;
 
-        std::string getTarget(void) const;
+    std::string getTarget(void) const;
 
-    private:
-        std::string const _target;
-        PresidentialPardonForm();
+private:
+    std::string const target_;
+    unsingned int requiredSign_;
+    unsingned int requiredExec_;
+    PresidentialPardonForm();
 
 };
 
