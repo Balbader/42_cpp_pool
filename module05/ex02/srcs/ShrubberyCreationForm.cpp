adff2dcad94e618a7f15dc72543e011c9ff9bf86
balbader <basilalbapro@gmail.com>
2024-01-05 16:32:27 +0100

    WIP

--- module05/ex02/srcs/ShrubberyCreationForm.cpp
+++ module05/ex02/srcs/ShrubberyCreationForm.cpp
@@ -39,6 +39,8 @@ ShrubberyCreationForm::ShrubberyCreationForm &operator=(const ShrubberyCreationF
     return *this;
 }
 
+std::ostream & operator<<(std::ostream &, ShrubberyCreationForm const &) {
+}
 
 // -------------------------------------------------------------------- Methods
 int ShrubberyCreationForm:: execute(Bureaucrat const & executor) const
@@ -65,37 +67,11 @@ int ShrubberyCreationForm:: execute(Bureaucrat const & executor) const
 			outputFile << "   ********* ********* ********* ********* ********* *********\n";
 			outputFile << "      ***       ***       ***       ***       ***       ***\n";
 			outputFile << "      ***       ***       ***       ***       ***       ***\n";
-		}-- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+		}
 	}
 	return 0;
 }
 
 
 // --------------------------------------------------------------------- Getter
-ShrubberyCreationForm::getTarget() {
-	return this->target_;
-}
+
