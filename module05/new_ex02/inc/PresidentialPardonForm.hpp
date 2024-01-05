69eaaaa148704e2978c2cf50b2137b1a69d90f5e
balbader <basilalbapro@gmail.com>
2024-01-05 09:46:15 +0100

    WIP

---
+++ module05/new_ex02/inc/PresidentialPardonForm.hpp
@@ -0,0 +1,16 @@
+#ifndef PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+
+class PresidentialPardonForm {
+public:
+	PresidentialPardonForm();
+	~PresidentialPardonForm();
+
+	PresidentialPardonForm(const PresidentialPardonForm &);
+	PresidentialPardonForm &operator=(const PresidentialPardonForm &);
+
+private:
+	
+};
+
+#endif // !PRESIDENTIALPARDONFORM_HPP
