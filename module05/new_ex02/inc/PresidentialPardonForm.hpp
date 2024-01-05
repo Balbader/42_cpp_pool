47a51624bef72c6aa4b02ddf05e353a157df9ac8
balbader <basilalbapro@gmail.com>
2024-01-05 14:00:18 +0100

    WIP

--- module05/new_ex02/inc/PresidentialPardonForm.hpp
+++ module05/new_ex02/inc/PresidentialPardonForm.hpp
@@ -1,15 +1,19 @@
 #ifndef PRESIDENTIALPARDONFORM_HPP
 #define PRESIDENTIALPARDONFORM_HPP
 
+#include "AForm.hpp"
+
 class PresidentialPardonForm {
 public:
 	PresidentialPardonForm();
+	PresidentialPardonForm(std::string, int, int);
 	~PresidentialPardonForm();
 
 	PresidentialPardonForm(const PresidentialPardonForm &);
 	PresidentialPardonForm &operator=(const PresidentialPardonForm &);
 
 private:
+	const std::string name_;
 	
 };
 
