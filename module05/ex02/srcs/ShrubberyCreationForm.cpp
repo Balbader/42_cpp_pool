e9c0caec0f0e0abd9933586b4964e51d7eec85aa
balbader <basilalbapro@gmail.com>
2024-01-05 14:25:56 +0100

    WIP

--- module05/ex02/srcs/ShrubberyCreationForm.cpp
+++ module05/ex02/srcs/ShrubberyCreationForm.cpp
@@ -7,11 +7,36 @@ ShrubberyCreationForm::ShrubberyCreationForm() : name_("Shrubbery Creation Form"
         std::cout << CYAN << "Shrubbery Creation Form base constructor called" << RESET << std::endl;
 }
 
-ShrubberyCreationForm::ShrubberyCreationForm(std::string name) : name_("Shrubbery Creation Form"), gradeToSign_(145), gradeToExec_(137) {
+ShrubberyCreationForm::ShrubberyCreationForm(std::string targetName) : name_(targetName), gradeToSign_(145), gradeToExec_(137) {
     if (DEBUG)
         std::cout << CYAN << "Shrubbery Creation Form base constructor called" << RESET << std::endl;
 }
 
+// ---------------------------------------------------------------- Destructor
+ShrubberyCreationForm::~ShrubberyCreationForm() {
+    if (DEBUG)
+        std::cout << CYAN << "Shrubbery Creation Form destructor calledaa"
+}
+
+// ----------------------------------------------------------- Copy Constructor
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const &rhs) {
+    if (DEBUG)
+        std::cout << CYAN << "Shrubbery Creation Form copy constructor called" << RESET << std::endl;
+
+    *this = rhs;
+}
+
+
+// -------------------------------------------------------------------- Overlaud
+ShrubberyCreationForm::ShrubberyCreationForm &operator=(const ShrubberyCreationForm &rhs) {
+    if (DEBUG)
+        std::cout << CYAN << "Shrubbery Creation Form assignment operator called" << RESET << std::endl;
+
+    it (*this != rhs)
+        this->isSigned = rhs.isSigned;
+
+    return *this;
+}
 
 int ShrubberyCreationForm:: execute(Bureaucrat const & executor) const
 {
