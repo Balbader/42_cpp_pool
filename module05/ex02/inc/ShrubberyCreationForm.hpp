4e255fc65817d3918d0e830136e24ea59415a0c8
balbader <basilalbapro@gmail.com>
2024-01-05 14:56:19 +0100

    WIP

--- module05/ex02/inc/ShrubberyCreationForm.hpp
+++ module05/ex02/inc/ShrubberyCreationForm.hpp
@@ -1,17 +1,42 @@
 #ifndef SHRUBBERYCREATIONFORM_HPP
 #define SHRUBBERYCREATIONFORM_HPP
 
+#include "AForm.hpp"
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <string>
+
 class ShrubberyCreationForm {
 public:
-  ShrubberyCreationForm();
-  ~ShrubberyCreationForm();
+	ShrubberyCreationForm(std::string, int, int);
+	~ShrubberyCreationForm();
 
-  ShrubberyCreationForm(const ShrubberyCreationForm &);
-  ShrubberyCreationForm &operator=(const ShrubberyCreationForm &);
+	ShrubberyCreationForm(const ShrubberyCreationForm &);
+	ShrubberyCreationForm &operator=(const ShrubberyCreationForm &);
+
+    virtual void beSigned(Bureaucrat const &);
+    virtual void execute(virtual const &) const;
+
+    std::string getTarget(void) const;
+
+    class FileException : public std::exception {
+    public:
+        virtual const char* what() const throw() {
+            return " creation has failed.\n";
+        }
+    };
+
+protected:
+    std::string const target_;
+	ShrubberyCreationForm();
 
 private:
-    unsigned int requiredSign = 145;
-    unsigned int requiredExec = 137;
+    int reqToSign_ = 145;
+    int reqToExec_ = 137;
+	
 };
+
+std::ostream & operator<<(std::ostream &, ShrubberyCreationForm const &);
 
 #endif // !SHRUBBERYCREATIONFORM_HPP
