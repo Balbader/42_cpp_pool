957a3555f7da5757df15a0b8a2a44b4fabb9a1e7
balbader <basilalbapro@gmail.com>
2024-01-05 06:52:54 +0100

    WIP

--- module05/ex02/srcs/RobotomyRequestForm.cpp
+++ module05/ex02/srcs/RobotomyRequestForm.cpp
@@ -1,3 +1,7 @@
 #include "RobotomyRequestForm.hpp"
 
 // ---------------------------------------------------------------- Constructor
+RobotomyRequestForm::RobotomyRequestForm() : name_("Robotomy Request Form"), gradeToSign_(72), gradeToExec_(45) {
+	if (DEBUG)
+		std::cout << YELLOW << "Robotomy Request Form base constructor called" << RESET << std::endl;
+}
