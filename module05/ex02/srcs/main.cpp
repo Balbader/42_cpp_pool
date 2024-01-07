281e37032e60db22a3501ebe3e2f61cabd612bf9
balbader <basilalbapro@gmail.com>
2024-01-07 22:01:30 +0100

    WIP

--- module05/ex02/srcs/main.cpp
+++ module05/ex02/srcs/main.cpp
@@ -1,134 +1,50 @@
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
 
-// void	create_a_president_form(std::string target)
-// {
-// 	Bureaucrat 				William;
-// 	Bureaucrat 				Hugo("Hugo", 1);
-// 	PresidentialPardonForm	form(target);
-
-// 	std ::cout << form;
-// 	std::cout << "\n";
-
-// 	try
-// 	{
-// 		form.execute(William);
-// 	}
-// 	catch (std::exception& e)
-// 	{
-// 		std::cerr << "Bureaucrat " << William.getName() << " couldn't execute " << form.getName() << " because " << e.what();
-// 	}
-// 	try
-// 	{
-// 		form.beSigned(William);
-// 	}
-// 	catch (std::exception& e)
-// 	{
-// 		std::cerr << "Bureaucrat " << William.getName() << " couldn't sign " << form.getName() << " because " << e.what();
-// 	}
-// 	William.signForm(form);
-// 	William.executeForm(form);
-
-// 	std::cout << "\n";
-// 	Hugo.signForm(form);
-// 	Hugo.executeForm(form);
-// 	Hugo.signForm(form);
-// }
-
-// void	create_a_shrubbery_form(std::string target)
-// {
-// 	Bureaucrat 				Gurvan;
-// 	Bureaucrat 				Meryem("Meryem", 1);
-// 	ShrubberyCreationForm	form(target);
-
-// 	std ::cout << form;
-// 	std::cout << "\n";
-
-// 	try
-// 	{
-// 		form.execute(Gurvan);
-// 	}
-// 	catch (std::exception& e)
-// 	{
-// 		std::cerr << "Bureaucrat " << Gurvan.getName() << " couldn't execute " << form.getName() << " because " << e.what();
-// 	}
-// 	try
-// 	{
-// 		form.beSigned(Gurvan);
-// 	}
-// 	catch (std::exception& e)
-// 	{
-// 		std::cerr << "Bureaucrat " << Gurvan.getName() << " couldn't sign " << form.getName() << " because " << e.what();
-// 	}
-// 	Gurvan.signForm(form);
-// 	Gurvan.executeForm(form);
-
-// 	std::cout << "\n";
-// 	Meryem.signForm(form);
-// 	Meryem.executeForm(form);
-// 	Meryem.signForm(form);
-// }
-
-// void	create_a_robotomy_form(std::string target)
-// {
-// 	Bureaucrat 				Axel;
-// 	Bureaucrat 				Karim("Karim", 1);
-// 	RobotomyRequestForm		form(target);
-
-// 	std ::cout << form;
-// 	std::cout << "\n";
-
-// 	try
-// 	{
-// 		form.execute(Axel);
-// 	}
-// 	catch (std::exception& e)
-// 	{
-// 		std::cerr << "Bureaucrat " << Axel.getName() << " couldn't execute " << form.getName() << " because " << e.what();
-// 	}
-// 	try
-// 	{
-// 		form.beSigned(Axel);
-// 	}
-// 	catch (std::exception& e)
-// 	{
-// 		std::cerr << "Bureaucrat " << Axel.getName() << " couldn't sign " << form.getName() << " because " << e.what();
-// 	}
-// 	Axel.signForm(form);
-// 	Axel.executeForm(form);
-
-// 	std::cout << "\n";
-// 	Karim.signForm(form);
-// 	Karim.executeForm(form);
-// 	Karim.signForm(form);
-// 	Karim.executeForm(form);
-// 	Karim.executeForm(form);
-// 	Karim.executeForm(form);
-// 	Karim.executeForm(form);
-// 	Karim.executeForm(form);
-// 	Karim.executeForm(form);
-// 	Karim.executeForm(form);
-// 	Karim.executeForm(form);
-// }
-
-int	main(int argc, char **argv)
+int	main(void)
 {
-	// if (argc != 2)
-	// {
-	// 	std::cerr << "Error: usage is ./Form <target>.\n";
-	// 	return (ERROR);
-	// }
-	// try
-	// {
-	// 	create_a_shrubbery_form(argv[1]);
-	// 	create_a_robotomy_form(argv[1]);
-	// 	create_a_president_form(argv[1]);
-	// }	
-	// catch (std::exception)
-	// {
-	// 	return (ERROR);
-	// }
-	// return (SUCCESS);
+	Bureaucrat zap1("Zap1", 1);
+	Bureaucrat bob20("Bob20", 20);
+	Bureaucrat dan70("Dan70", 70);
+	Bureaucrat ted100("Ted100", 100);
+	Bureaucrat tod150("Tod150", 150);
+	std::cout << std::endl;
+	{
+		ShrubberyCreationForm scf("Planet2000Express");
+		tod150.executeForm(scf);
+		ted100.executeForm(scf);
+		tod150.signForm(scf);
+		ted100.signForm(scf);
+		tod150.executeForm(scf);
+		ted100.executeForm(scf);
+	}
+		std::cout << std::endl;
+	{
+		RobotomyRequestForm rrf("Fry");
+		ted100.executeForm(rrf);
+		dan70.executeForm(rrf);
+		ted100.signForm(rrf);
+		dan70.signForm(rrf);
+		dan70.executeForm(rrf);
+		bob20.executeForm(rrf);
+		bob20.executeForm(rrf);
+		bob20.executeForm(rrf);
+		bob20.executeForm(rrf);
+		bob20.executeForm(rrf);
+	}
+		std::cout << std::endl;
+	{
+		PresidentialPardonForm ppf("Bender");
+		bob20.executeForm(ppf);
+		zap1.executeForm(ppf);
+		dan70.signForm(ppf);
+		bob20.signForm(ppf);
+		bob20.executeForm(ppf);
+		zap1.executeForm(ppf);
+	}
+	std::cout << std::endl;
 	return (0);
 }
