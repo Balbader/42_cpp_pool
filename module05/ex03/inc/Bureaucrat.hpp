69adc7c066e6fff311b36efb89348acbb246de0e
balbader <basilalbapro@gmail.com>
2024-01-08 18:00:22 +0100

    WIP

---
+++ module05/ex03/inc/Bureaucrat.hpp
@@ -0,0 +1,54 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include "AForm.hpp"
+#include "colors.hpp"
+#include <iostream>
+#include <string>
+
+#define DEBUG 1
+
+class AForm;
+
+class Bureaucrat {
+public:
+	Bureaucrat(const std::string &, int);
+	~Bureaucrat();
+
+	Bureaucrat(const Bureaucrat &);
+	Bureaucrat &operator=(const Bureaucrat &);
+
+	class GradeTooHighException : public std::exception {
+		public:
+			virtual const char* what() const throw() {
+				return "Error Occured!\nGrade too high.\n";
+		}
+	};
+
+	class GradeTooLowException : public std::exception {
+		public:
+			virtual const char* what() const throw() {
+				return "Error Occured!\nGrade too low.\n";
+		}
+	};
+
+	void incrementGrade();
+	void decrementGrade();
+
+	void signForm(AForm&);
+	void execForm(AForm&);
+
+	const std::string getName() const;
+	int getGrade() const;
+
+protected:
+	Bureaucrat();
+
+private:
+	const std::string name_;
+	int grade_;
+};
+
+std::ostream& operator<<(std::ostream& lhs, const Bureaucrat& rhs);
+
+#endif // !BUREAUCRAT_HPP
---
+++ module05/ex03/srcs/Bureaucrat.cpp
@@ -0,0 +1,129 @@
+#include "Bureaucrat.hpp"
+
+// ----------------------------------------------------------------------------
+// ---------------------------------------------------------------- Constructor
+// ----------------------------------------------------------------------------
+Bureaucrat::Bureaucrat() : name_("Name undefined"), grade_(10) {
+  if (DEBUG)
+    std::cout << GREEN << "Bureaucrat Base Constructor called" << RESET
+              << std::endl;
+}
+
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------- Args Constructor
+// ----------------------------------------------------------------------------
+Bureaucrat::Bureaucrat(const std::string& name, int grade)
+		   : name_(name), grade_(grade) {
+  if (DEBUG)
+    std::cout << GREEN << "Bureaucrat Arguments Base Constructor called"
+              << RESET << std::endl;
+
+	// if (grade < 1)
+	// 	throw Bureaucrat::GradeTooHighException();
+	// else if (grade > 150)
+	// 	throw Bureaucrat::GradeTooLowException();
+
+	try {
+		if (grade < 1)
+			throw GradeTooHighException();
+		if (grade > 150)
+			throw GradeTooLowException();
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------- Copy Constructor
+// ----------------------------------------------------------------------------
+Bureaucrat::Bureaucrat(const Bureaucrat& rhs) {
+  if (DEBUG)
+    std::cout << GREEN << "Bureaucrat Copy Constructor called"
+              << RESET << std::endl;
+
+	*this = rhs;
+}
+
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------- Destructor
+// ----------------------------------------------------------------------------
+Bureaucrat::~Bureaucrat() {
+  if (DEBUG)
+    std::cout << GREEN << "Bureaucrat base destructor called" << RESET << std::endl;
+}
+
+
+// ----------------------------------------------------------------------------
+// ------------------------------------------------------------------- Overload
+// ----------------------------------------------------------------------------
+Bureaucrat&	Bureaucrat::operator=(const Bureaucrat& rhs) {
+	if (DEBUG)
+    std::cout << ORANGE << "AForm assignment operator overload called" << RESET << std::endl;
+
+	if (this != &rhs) {
+		this->grade_ = rhs.grade_;
+	}
+
+	return (*this);
+}
+
+
+// ----------------------------------------------------------------------------
+// -------------------------------------------------------------------- Methods
+// ----------------------------------------------------------------------------
+void Bureaucrat::signForm(AForm& form) {
+	try {
+		form.beSigned(*this);
+		std::cout << this->getName() << " signed " << form.getName() << std::endl;
+	}
+	catch(const std::exception& e) {
+		std::cerr << this->getName() << " couldn't sign " << form.getName() << " because " 
+		<< e.what() << std::endl;
+	}
+}
+
+void Bureaucrat::execForm(AForm& form) {
+	try {
+		form.execute(*this);
+		std::cout << this->getName() << " executed " << form.getName() << std::endl;
+	}
+	catch(const std::exception& e) {
+		std::cerr << this->getName() << " couldn't execute " << form.getName() << " because " 
+		<< e.what() << std::endl;
+	}
+}
+
+void Bureaucrat::incrementGrade() {
+	if (grade_ - 1 < 1)
+		throw Bureaucrat::GradeTooHighException();
+
+	grade_ -= 1;
+}
+
+void Bureaucrat::decrementGrade() {
+	if (grade_ + 1 > 150)
+		throw Bureaucrat::GradeTooLowException();
+
+	grade_ += 1;
+}
+
+
+// ----------------------------------------------------------------------------
+// -------------------------------------------------------------------- Getters
+// ----------------------------------------------------------------------------
+const std::string Bureaucrat::getName() const { return (this->name_); }
+int	Bureaucrat::getGrade() const { return (this->grade_); }
+
+
+// ----------------------------------------------------------------------------
+// -------------------------------------------------------------------- ostream
+// ----------------------------------------------------------------------------
+std::ostream& operator<<(std::ostream& lhs, const Bureaucrat& rhs) {
+
+	lhs << rhs.getName() << " bureaucrat grade " << rhs.getGrade() << std::endl;
+
+	return lhs;
+}
