1ee712e21a481e928f66f1d4b30ab99bdc3b5f34
balbader <basilalbapro@gmail.com>
2024-01-08 13:39:57 +0100

    WIPs

--- module05/new_ex02/inc/Bureaucrat.hpp
+++ module05/new_ex02/inc/Bureaucrat.hpp
@@ -8,4 +8,45 @@
 
 class AForm;
 
+class Bureaucrat {
+public:
+	Bureaucrat(const std::string &, int);
+	~Bureaucrat();
+
+	Bureaucrat(const Bureaucrat &) = default;
+	Bureaucrat &operator=(const Bureaucrat &) = default;
+
+	class GradeTooHighException : public std::exception {
+		public:
+			virtual const char* what() const throw() {
+				return "Error Occured!\nGrade too high.\n";
+		}
+	};
+
+	class GradeTooLowException : public std::exception {
+		public:
+			virtual const char* what() const throw() {
+				return "Error Occured!\nGrade too low.\n";
+		}
+	};
+
+	void incrementGrade();
+	void decrementGrade();
+
+	void signForm(AForm&);
+	void execForm(AForm&);
+
+	const std::string getName() const;
+	int getGrade() const;
+
+protected:
+	Bureaucrat();
+
+private:
+	const std::string name_;
+	int grade_;
+};
+
+std::ostream& operator<<(std::ostream& lhs, const Bureaucrat& rhs);
+
 #endif // !BUREAUCRAT_HPP
