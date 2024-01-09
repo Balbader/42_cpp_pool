b70bc8eea4ac211147207ae7518e71a3643900ec
balbader <basilalbapro@gmail.com>
2024-01-09 11:36:08 +0100

    WIP

---
+++ module07/ex00/inc/Functions.hpp
@@ -0,0 +1,18 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+#include "colors.hpp"
+
+#include <iostream>
+#include <string>
+
+template<typename T>
+void swap(T& a, T& b);
+
+template<typename T>
+T& min(T& a, T& b);
+
+template<typename T>
+T& max(T& a, T& b);
+
+#endif
