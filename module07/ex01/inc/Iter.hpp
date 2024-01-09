76c88f1ca67670f28f31155e4cbc23e5e61ab08c
balbader <basilalbapro@gmail.com>
2024-01-09 16:08:30 +0100

    WIP

---
+++ module07/ex01/inc/Iter.hpp
@@ -0,0 +1,23 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <iostream>
+#include <string>
+
+template<typename T>
+void iter(T* array, size_t length, void(*f)(const T&)) {
+    if (array) {
+        for (size_t i = 0; i < length; i++)
+            f(array[i]);
+    }
+    else
+        std::cout << "Stop trying to break my program" << std::endl;
+}
+
+//no need for protection because reference and T* not T**
+template<typename T>
+void print(const T& c) {
+    std::cout << c << std::endl;
+}
+
+#endif
