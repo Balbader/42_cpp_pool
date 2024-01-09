3c6ac727e841ab6fff80d54a6e7acd47eeaf74c9
balbader <basilalbapro@gmail.com>
2024-01-09 12:14:08 +0100

    WIP

---
+++ module07/ex00/inc/Functions.hpp
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+#include "Swap.hpp"
+#include "Max.hpp"
+#include "Min.hpp"
+#include "colors.hpp"
+
+#include <iostream>
+
+#endif // !FUNCTIONS_HPP
