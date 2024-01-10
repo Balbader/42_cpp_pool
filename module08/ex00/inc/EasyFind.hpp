895cff877332290d506426b4498cfd6f4459de6e
balbader <basilalbapro@gmail.com>
2024-01-10 05:57:42 +0100

    WIP

---
+++ module08/ex00/inc/EasyFind.hpp
@@ -0,0 +1,20 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template<typename T>
+
+typename T::iterator easyfind(T& container, int toFind) {
+    typename T::iterator it = std::find(container.begin(), container.end(), toFind);
+
+    if (it == container.end())
+        throw std::invalid_argument("Argument invalide");
+
+    return it;
+    //if not found it returns the container.end()
+}
+
+#endif
