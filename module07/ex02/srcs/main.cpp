a847d9dd32045605d4dbea86b75ea9e4ec0a1777
balbader <basilalbapro@gmail.com>
2024-01-11 08:14:26 +0100

    WIP

--- module07/ex02/srcs/main.cpp
+++ module07/ex02/srcs/main.cpp
@@ -22,79 +22,80 @@ int main(void)
     //     std::cout << "Mirror:"<< mirror[i] << std::endl;
     // }
 
-    std::cout << "\nTest Copie Profonde ===================================== \n" << std::endl;
     //SCOPE TEST COPIE PROFONDE
-    {
+    std::cout << "\nTest Copie Profonde ===================================== \n" << std::endl; {
+
         std::cout << "Testons d'abord la surcharge de l'operateur =" << std::endl;
+
         Array<int> toCopy(10);
-        for (unsigned int i = 0; i < toCopy.size(); i++)
-        {
+
+        for (unsigned int i = 0; i < toCopy.size(); i++) {
             toCopy[i] = i;
         }
+
         Array<int> copy = toCopy;
+
         std::cout << "Maintenant, affichons les deux Arrays" << std::endl;
-        for (unsigned int i = 0; i < toCopy.size(); i++)
-        {
+		
+        for (unsigned int i = 0; i < toCopy.size(); i++) {
             std::cout << "toCopy[" << i << "]: " << toCopy[i] << std::endl;
             std::cout << "copy[" << i << "]: " << copy[i] << std::endl;
         }
+
         std::cout << "Modifions copy : cela ne devrait pas modifier toCopy!" << std::endl;
-        for (unsigned int i = 0; i < toCopy.size(); i++)
-        {
+
+        for (unsigned int i = 0; i < toCopy.size(); i++) {
             copy[i] = 10 - i;
         }
+
         std::cout << "copy modifie, affichons les deux Array a nouveau" << std::endl;
-        for (unsigned int i = 0; i < toCopy.size(); i++)
-        {
+
+        for (unsigned int i = 0; i < toCopy.size(); i++) {
             std::cout << "toCopy[" << i << "]: " << toCopy[i] << std::endl;
             std::cout << "copy[" << i << "]: " << copy[i] << std::endl;
         }
 
         std::cout << "Maintenant, testons la copie par constructeur" << std::endl;
+
         Array<int> copy2(copy);
-        for (unsigned int i = 0; i < copy.size(); i++)
-        {
+
+        for (unsigned int i = 0; i < copy.size(); i++) {
             std::cout << "copy[" << i << "]: " << copy[i] << std::endl;
             std::cout << "copy2[" << i << "]: " << copy2[i] << std::endl;
         }
+
         std::cout << "Modifions copy2 : cela ne devrait pas modifier copy!" << std::endl;
-        for (unsigned int i = 0; i < toCopy.size(); i++)
-        {
+
+        for (unsigned int i = 0; i < toCopy.size(); i++) {
             copy2[i] = i;
         }
+
         std::cout << "copy2 modifie, affichons les deux Array a nouveau" << std::endl;
-        for (unsigned int i = 0; i < copy.size(); i++)
-        {
+
+        for (unsigned int i = 0; i < copy.size(); i++) {
             std::cout << "copy[" << i << "]: " << copy[i] << std::endl;
             std::cout << "copy2[" << i << "]: " << copy2[i] << std::endl;
         }
     }
 
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
+    for (int i = 0; i < MAX_VAL; i++) {
+        if (mirror[i] != numbers[i]) {
             std::cerr << "didn't save the same value!!" << std::endl;
             return 1;
         }
     }
     
     std::cout << "Quelques tests de l'index out of bound" << std::endl;
-    try
-    {
+
+    try {
         numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
+	} catch(const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
-    try
-    {
+
+    try {
         numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
+    } catch(const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
 
