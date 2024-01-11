fdb98739887b8db2e1bd5ff1b0aedc78dac88ab7
balbader <basilalbapro@gmail.com>
2024-01-11 08:48:15 +0100

    WIP

--- module07/ex02/inc/Array.hpp
+++ module07/ex02/inc/Array.hpp
@@ -7,24 +7,74 @@
 
 template<typename T>
 
-//FIX: check how to define the class parameters in a different file
-class Array {
+class Array
+{
 public:
-	Array(unsigned int);
-	~Array();
+	// constructor
+	Array(unsigned int n) {
+		tabSize = n;
+		tab = new T[n];
 
-	Array(const Array&);
-	Array& operator=(const Array&);
+		for(unsigned int i = 0; i < n; i++) {
+			tab[i] = 0;
+		}
+	}
 	
-	T& operator[](unsigned int);
-	const T& operator[](unsigned int) const;
-	unsigned int getSize() const;
+	// copy constructor
+	Array(const Array& rhs) {
+
+		this->tabSize = rhs.size();
+		this->tab = new T[rhs.size()];
+
+		for(unsigned int i = 0; i < rhs.size(); i++) {
+			this->tab[i] = rhs[i]; //good si object bicoz appelle surcharge de l'operateur = 
+		}
+	}
+
+	// destructor
+	~Array() {
+		delete [] tab;
+	}
+
+	// assignment operator
+	Array& operator=(const Array& rhs) {
+		delete [] tab; //very deep copy
+		tabSize = rhs.size();
+		tab = new T[rhs.size()];
+
+		for (unsigned int i = 0; i < rhs.size(); i++) {
+			tab[i] = rhs[i];
+		}
+
+		return (*this);
+	}
+	
+	// methods
+	//unsigned int, if a negative number is sent it will automatically overflow
+	T& operator[](unsigned int index) {
+		if (index >= this->tabSize) 
+			throw std::out_of_range("Out of range exception");
+		return (this->tab[index]);
+	}
+
+	const T& operator[](unsigned int index) const {
+		if (index >= this->tabSize)
+			throw std::out_of_range("Out of range exception");
+		return (this->tab[index]);
+	}
+	
+	unsigned int size() const {
+		return (this->tabSize);
+	}
 
 private:
 	T* tab;
-	unsigned int sizeTab;
+	unsigned int tabSize;
 
-	Array();
+	Array() {
+		tabSize = 0;
+		tab = new T[0];
+	}
 };
 
 #endif
