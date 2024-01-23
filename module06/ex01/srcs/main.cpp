cadbc4c36a913d43c4168367975ed45432a366c8
Basil ALBADER <baalbade@student.42.fr>
2024-01-23 10:12:28 +0100

    WIP

--- module06/ex01/srcs/main.cpp
+++ module06/ex01/srcs/main.cpp
@@ -4,11 +4,13 @@ int main(void)
 {
     Data* greetings = new Data;
 	greetings->str = "Hello Serialization World!";
-    std::cout << "Base ptr : " << LBLUE << greetings->str << RESET << std::endl;
-    std::cout << "Base ptr address : " << LBLUE << greetings << RESET << std::endl;
+    std::cout << "Base ptr : " << LGREEN << greetings->str << RESET << std::endl;
+    std::cout << "Base ptr address : " << LGREEN << greetings << RESET << std::endl;
+
+    std::cout << "\n";
 
     uintptr_t rawGreeting = Serialization::serialize(greetings);
-    std::cout << "uintptr address after Serialization : " << LYELLOW << rawGreeting << RESET << "\n" << std::endl;
+    std::cout << "uintptr address after Serialization : " << ORANGE << rawGreeting << RESET << "\n" << std::endl;
 
     //Fetch original ptr Deserialization
     Data* goodbye = Serialization::deserialize(rawGreeting);
