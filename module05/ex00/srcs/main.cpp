cef06bf365e697cfa629f0cb1e5b7274371348cc
Basil ALBADER <baalbade@student.42.fr>
2024-01-23 08:17:46 +0100

    WIP

--- module05/ex00/srcs/main.cpp
+++ module05/ex00/srcs/main.cpp
@@ -3,9 +3,13 @@
 int main() {
   std::cout << "\n";
   Bureaucrat mimi("mimi", 448);
+  std::cout << "mimi\n" << mimi << "\n";
+
   Bureaucrat riri("riri", -31);
+  std::cout << "riri\n" << riri << "\n";
+
   Bureaucrat fifi("fifi", 42);
-  std::cout << fifi;
+  std::cout << "fifi\n" << fifi;
 
   return 0;
 }
