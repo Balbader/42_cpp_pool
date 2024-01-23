10acefa9d84d09792df8b0b583b918b637f4b767
Basil ALBADER <baalbade@student.42.fr>
2024-01-23 08:54:28 +0100

    WIP

--- module06/ex01/srcs/main.cpp
+++ module06/ex01/srcs/main.cpp
@@ -4,16 +4,16 @@ int main(void)
 {
     Data* greetings = new Data;
 	greetings->str = "Hello Serialization World!";
-    std::cout << "Base ptr : " << greetings->str << std::endl;
-    std::cout << "Base ptr address : " << greetings << "\n" << std::endl;
+    std::cout << "Base ptr : " << LBLUE << greetings->str << RESET << std::endl;
+    std::cout << "Base ptr address : " << LBLUE << greetings << RESET << std::endl;
 
     uintptr_t rawGreeting = Serialization::serialize(greetings);
-    std::cout << "uintptr address after Serialization : " << rawGreeting << "\n" << std::endl;
+    std::cout << "uintptr address after Serialization : " << LYELLOW << rawGreeting << RESET << "\n" << std::endl;
 
     //Fetch original ptr Deserialization
     Data* goodbye = Serialization::deserialize(rawGreeting);
-    std::cout << "Deserialized ptr : " << goodbye->str << std::endl;
-    std::cout << "Deserialized ptr address : " << goodbye << std::endl;
+    std::cout << "Deserialized ptr : " << LGREEN << goodbye->str << RESET << std::endl;
+    std::cout << "Deserialized ptr address : " << LGREEN << goodbye << RESET << std::endl;
     
     delete greetings;
     return (0);
