7b1ae11c890b5d2bb099d12ee039d9e05bfaa2b5
Basil ALBADER <baalbade@student.42.fr>
2024-01-26 15:23:23 +0100

    WIP

--- module05/ex03/srcs/main.cpp
+++ module05/ex03/srcs/main.cpp
@@ -1,41 +1,95 @@
-
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
+#include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
 #include "Intern.hpp"
 
-void	intern_creation(std::string form_name, std::string target)
-{
-	Intern	Heidi;
-	AForm	*Form;
 
-	try
-	{
-		Form = Heidi.makeForm("Presidential pardon", "Lisa");
-		delete Form;
-		Form = Heidi.makeForm("Robotomy request", "Heloise");
-		delete Form;
-		Form = Heidi.makeForm("Shrubbery creation", "Home");
-		delete Form;
-		Form = Heidi.makeForm(form_name, target);
-		if (Form)
-			delete Form;
-	}
-	catch(const std::exception)
-	{
-		std::cerr << "Failed. Available forms are : Presidential pardon, Robotomy request and Shrubbery creation.\n";
-	}
-}
-
-int	main(int argc, char **argv)
+int main(void)
 {
-	if (argc != 3)
-	{
-		std::cerr << "Error: usage is ./intern <name of form> <target>.\n";
-		return (ERROR);
-	}
-	intern_creation(argv[1], argv[2]);
-	return (SUCCESS);
+     std::cout << "\n-------------------CEO----------------------------------" << std::endl;
+    try
+    {
+        Bureaucrat bur = Bureaucrat("Sophie Vigier", 1);
+	    Intern pipo = Intern();
+
+		AForm* form = pipo.makeForm("shrubbery creation", "42Paris");
+		if (!form)
+		{
+			std::cout << "Error creating form, abort" << std::endl;
+			return (1);
+		}
+	    std::cout << bur;
+        std::cout << *form;
+        
+        bur.signForm(*form);
+        bur.execForm(*form);
+        bur.execForm(*form);
+        bur.execForm(*form);
+
+        AForm* dead = pipo.makeForm("robotomy request", "baalbade");
+		if (!dead)
+		{
+			std::cout << "Error creating form, abort" << std::endl;
+			delete form;
+			return (1);
+		}
+        std::cout << *dead;
+
+        bur.signForm(*dead);
+        bur.execForm(*dead);
+        
+        AForm* mercy = pipo.makeForm("presidential pardon", "baalbade");
+		if (!mercy)
+		{
+			std::cout << "Error creating form, abort" << std::endl;
+			delete form;
+			delete dead;
+			return (1);
+		}
+        std::cout << *mercy;
+        bur.signForm(*mercy);
+        bur.execForm(*mercy);
+		delete form;
+		delete dead;
+		delete mercy;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "\n-------------------LIL MAN-------------------------" << std::endl;
+    try
+    {
+        Bureaucrat bur = Bureaucrat("Useless Intern", 150);
+	    ShrubberyCreationForm form("Home");
+	
+	    std::cout << bur;
+        std::cout << form;
+        
+        bur.signForm(form);
+        bur.execForm(form);
+        bur.execForm(form);
+        bur.execForm(form);
+
+        RobotomyRequestForm dead("baalbade");
+        std::cout << dead;
+
+        bur.signForm(dead);
+        bur.execForm(dead);
+        
+        PresidentialPardonForm mercy("baalbade");
+        std::cout << mercy;
+        bur.signForm(mercy);
+        bur.execForm(mercy);
+        
+        std::cout << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    
+	
 }
