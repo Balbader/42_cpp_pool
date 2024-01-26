06eef27aeed1dc2d5113bbffecd7ca4282516e0f
Basil ALBADER <baalbade@student.42.fr>
2024-01-26 14:30:53 +0100

    WIP

--- module05/ex00/inc/Exceptions.hpp
+++ module05/ex00/inc/Exceptions.hpp
@@ -4,16 +4,16 @@
 #include <exception>
 #include <iostream>
 
-  class GradeTooHighException : public std::exception {
-      virtual const char* what() const throw() {
+class GradeTooHighException : public std::exception {
+    virtual const char* what() const throw() {
         return "Exception error occured.\nGrade too high.\n";
     }
-  };
-  
-  class GradeTooLowException : public std::exception {
-      virtual const char* what() const throw() {
+};
+
+class GradeTooLowException : public std::exception {
+    virtual const char* what() const throw() {
         return "Exception error occured.\nGrade too Low.\n";
     }
-  };
+};
 
 #endif // !EXCEPTIONS_HPP
