a38fad72f96501f5f3cfbbaf41743b6cdc4cf493
Basil ALBADER <baalbade@student.42.fr>
2024-02-19 12:17:30 +0100

    WIP

--- module08/ex02/inc/Mutants.hpp
+++ module08/ex02/inc/Mutants.hpp
@@ -36,14 +36,6 @@ class Mutants : public std::stack<T> {
         iterator end() { 
             return this->c.end(); 
         }
-
-        const_iterator begin() const { 
-            return this->c.begin(); 
-        }
-
-        const_iterator end() const { 
-            return this->c.end(); 
-        }
 };
 
 
