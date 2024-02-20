8b5cf2d240be43526fa54f2c0059fe89fc0270bd
Basil ALBADER <baalbade@student.42.fr>
2024-02-20 10:15:13 +0100

    WIP

--- module09/ex00/srcs/BitcoinExchange.cpp
+++ module09/ex00/srcs/BitcoinExchange.cpp
@@ -29,7 +29,7 @@ BitcoinExchange::~BitcoinExchange()
 // ------------------------------------------------------------------- Overload
 // ----------------------------------------------------------------------------
 
-BitcoinExchange &	BitcoinExchange::operator=(BitcoinExchange const & rhs)
+BitcoinExchange & BitcoinExchange::operator=(BitcoinExchange const & rhs)
 {
 	if (DEBUG)
 		std::cout << LBLUE << "BitcoinExchange assignment operator called." << RESET << std::endl;
@@ -76,36 +76,37 @@ const char* ParseFailException::what() const throw()
 // -------------------------------------------------------------------- Methods
 // ----------------------------------------------------------------------------
 
-void	BitcoinExchange::getDatabase(void)
+void BitcoinExchange::getDatabase(void)
 {
+    // open the file
 	std::ifstream infile("data.csv");
-
-    // Check for file errors
+    
+    // check if file was open successfully or if there is an error
 	if (!infile.is_open() || infile.fail())
 		throw(FileOpeningException());
 
+    // reading the file
 	std::string line;
 	while (std::getline(infile, line))
 	{
-        // Parsing data
+        // date = 10 first characters + extract date
 		std::string date = line.substr(0, 10);
+
+        // value starts on char 11 + extract value
 		std::string valueStr = line.substr(11, (int)line.size());
 
-        // Extract data 
+        // parse && store value
 		std::istringstream	iss(valueStr);
 		char *valueNbr = new char[valueStr.length() + 1];
 		std::strcpy(valueNbr, valueStr.c_str());
 
+        // convert value to double
 		if (iss >> valueNbr)
 		{
-            // convert string to double
 			double valueDouble = atof(valueNbr);
-
-            // insert data to database by generating key/value pairs
 			this->_database.insert(std::make_pair(date, valueDouble));
 			delete[]valueNbr;
 		}
-
 		else
 		{
 			delete[] valueNbr;
@@ -113,20 +114,26 @@ void	BitcoinExchange::getDatabase(void)
 		}
 	}
 
+    // close file
 	infile.close();
 }
 
-void	BitcoinExchange::checkDate(std::string line)
+void BitcoinExchange::checkDate(std::string line)
 {
 	bool isLeapYear = FALSE;
+
+    // convert string to int
 	int	Year = atoi(line.substr(0,4).c_str());
 
+    // check if year is divisible by 4 || 400 but not 100
 	if ((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0))
 		isLeapYear = TRUE;
 
+    // extract month && day
 	int	Month = atoi(line.substr(5,2).c_str());
 	int	Day = atoi(line.substr(8,2).c_str());
 
+    // Date validation
 	if ((Month < 1 || Month > 12) || (Day < 1 || Day > 31))
 		throw (WrongDateFormatException());
 
@@ -139,7 +146,7 @@ void	BitcoinExchange::checkDate(std::string line)
 
 
 // make sure that data is at the right format
-void	BitcoinExchange::checkFormat(std::string line)
+void BitcoinExchange::checkFormat(std::string line)
 {
 	std::string date = line.substr(0, 10);
 
@@ -174,8 +181,9 @@ void	BitcoinExchange::checkFormat(std::string line)
 }
 
 // make sure that BitcoinExchange value format is right
-void	BitcoinExchange::checkValue(std::string line)
+void BitcoinExchange::checkValue(std::string line)
 {
+    // count positive signs
 	int nbPositiveSign = 0;
 	for (int i = 13; i < (int)line.size(); i++)
 	{
@@ -185,11 +193,15 @@ void	BitcoinExchange::checkValue(std::string line)
 			break;
 	}
 
+    // extract the value, starts immediately after any + sign
 	std::string value = line.substr(13 + nbPositiveSign, line.size());
+
+    // check for dots and non digit characters
 	int cptDot = 0;
 	for (int i = 0; i < (int)value.size(); i++)
 	{
 
+        // count the number of '.'
 		if (value[i] == '.')
 		{
 			cptDot += 1;
@@ -197,19 +209,24 @@ void	BitcoinExchange::checkValue(std::string line)
 				throw(WrongValueFormatException());
 		}
 
+        // except for the decimal point, make sure that every character is a digit
 		else if (!isdigit(value[i]))
 			throw(WrongValueFormatException());
 	}
 
+    // make sure that a valid value is a four digit integer
+    // or a decimal number with no more than four digits before the decimal point
 	if (cptDot == 0 && value.size() > 4)
 		throw(WrongValueFormatException());
 
     // extract data and check data
 	std::istringstream iss(value);
+    
+    // store data into a dynamically allocated array
 	char *valueNbr = new char[value.length() + 1];
 	std::strcpy(valueNbr, value.c_str());
 
-    // if data was properly extracted
+    // check if data is in range 0 - 1000
 	if (iss >> valueNbr)
 	{
 		if (atoi(valueNbr) < 0 || atoi(valueNbr) > 1000)
@@ -225,17 +242,21 @@ void	BitcoinExchange::checkValue(std::string line)
 		throw(ParseFailException());
 	}
 
+    // deallocate the allocated memory
 	delete[] valueNbr;
 }	
 
 
-void	BitcoinExchange::getInput(char *input)
+void BitcoinExchange::getInput(char *input)
 {
-    // check input file stream
+    // open the input file
 	std::ifstream infile(input);
+
+    // check if file is open
 	if (!infile.is_open() || infile.fail())
 		throw(FileOpeningException());
 
+    // read line from the file
 	std::string line;
 	while (std::getline(infile, line))
 	{
@@ -248,15 +269,17 @@ void	BitcoinExchange::getInput(char *input)
 			std::cerr << "Error: " << e.what() << '\n';
 		}
 	}
+
+    // close the file
 	infile.close();
 }
 
-void	BitcoinExchange::printValue(std::string line)
+void BitcoinExchange::printValue(std::string line)
 {
 	std::string date = line.substr(0, 10);
 	double amountBitcoin = atof(line.substr(13).c_str());
-	map_t::const_iterator	it = this->_database.find(date);
-	map_t::const_iterator	end = this->_database.end();
+	map_t::const_iterator it = this->_database.find(date);
+	map_t::const_iterator end = this->_database.end();
 	if (it == end)
 	{
 		it = this->_database.upper_bound(date);
